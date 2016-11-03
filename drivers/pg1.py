efe0f9b9d3ba04adad11554746d856b5fe1781f8
Ruo Ando <ando.ruo@gmail.com>
2016-11-03 16:09:12 +0900

    pg test

--- drivers/pg1.py
+++ drivers/pg1.py
@@ -11,8 +11,27 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-cur.execute("INSERT INTO test (id, name) VALUES (%s, %s)", (1, "test"))
+sql = "drop TABLE test;"
+cur.execute(sql)
 
+sql = "create TABLE test(id INT, name varchar(40));"
+cur.execute(sql)
+
+sql = "INSERT INTO test (id, name) VALUES (3, 'hanako');"
+cur.execute(sql)
+
+sql = "select * from test"
+cur.execute(sql)
+
+ans =cur.fetchall()
+print ans
+
+#dict_result = []
+#for row in ans:
+#    dict_result.append(dict(row))
+
+#print dict_result
+        
 conn.commit()
 cur.close()
 conn.close()
