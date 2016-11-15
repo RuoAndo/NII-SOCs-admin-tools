01c00154e01fb89237dfcd242c4f499277deb707
Ruo Ando <ando.ruo@gmail.com>
2016-11-15 11:11:46 +0900

    explain

---
+++ drivers/explain1.py
@@ -0,0 +1,18 @@
+import sys 
+import psycopg2
+
+
+conn = psycopg2.connect(
+    host = "192.168.1.1",
+    port = 5432,
+    database="sample",
+    user="postgres",
+    password="")
+
+cur = conn.cursor()
+
+sql = "explain select count(*) from test;"
+cur.execute(sql)
+
+stats = cur.fetchall()
+print stats
