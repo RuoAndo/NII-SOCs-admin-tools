6a8c06521846f0fd0d1ff027a6043a9e60c10ec7
root <root@ip-10-0-1-29.ap-northeast-1.compute.internal>
2016-12-02 00:49:36 -0500

    auto

--- cassandra/install.sh
+++ cassandra/install.sh
@@ -2,3 +2,7 @@ yum install -y jna
 yum install -y java-1.7.0*
 cp datasax.repo /etc/yum.repos.d/
 yum -y install cassandra20
+curl -kL https://bootstrap.pypa.io/get-pip.py | python
+pip install cassandra-driver
+cp cassandra.yaml /etc/cassandra/conf/cassandra.yaml
+/etc/init.d/cassandra restart
