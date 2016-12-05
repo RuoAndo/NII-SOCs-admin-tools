e1ad6c4181a154048c3c9db008ffedaf0940e260
RuoAndo <ando.ruo@gmail.com>
2016-12-05 17:33:17 +0000

    xrdp

--- livedvd/vnc.sh
+++ livedvd/vnc.sh
@@ -1,7 +1,10 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get update
 sudo -E apt-get install -y ubuntu-desktop
-sudo apt-get install xfce4 xrdp
-sudo apt-get install xfce4 xfce4-goodies
+sudo apt-get install -y xfce4 xrdp
+sudo apt-get install -y xfce4 xfce4-goodies
 echo xfce4-session > ~/.xsession
 sudo cp /home/ubuntu/.xsession /etc/skel
+\cp -f xrdp.ini /etc/xrdp/xrdp.ini
+sudo apt-get install -y vnc4server
+sudo service xrdp restart
