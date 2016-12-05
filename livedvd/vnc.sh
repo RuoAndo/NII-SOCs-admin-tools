a64e6f5e1ce67503de9f7cc0464beae3ac770c55
RuoAndo <ando.ruo@gmail.com>
2016-12-05 19:46:12 +0000

    vnc

--- livedvd/vnc.sh
+++ livedvd/vnc.sh
@@ -1,22 +1,22 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get update
 sudo -E apt-get install -y ubuntu-desktop
-sudo apt-get install -y xfce4 xrdp
+#sudo apt-get install -y xfce4 xrdp
 sudo apt-get install -y xfce4 xfce4-goodies
-#cd
-#mkdir src
-#cd src
-#wget https://github.com/neutrinolabs/xrdp/archive/devel.zip
-#unzip devel.zip
-#cd xrdp-devel
-#sudo apt-get install -y autoconf libtool libssl-dev libpam0g-dev libx11-dev libxfixes-dev libxrandr-dev
-#./bootstrap
-#./configure
-#make
-#sudo make install
+cd
+mkdir src
+cd src
+wget https://github.com/neutrinolabs/xrdp/archive/devel.zip
+unzip devel.zip
+cd xrdp-devel
+sudo apt-get install -y autoconf libtool libssl-dev libpam0g-dev libx11-dev libxfixes-dev libxrandr-dev
+./bootstrap
+./configure
+make
+sudo make install
 echo xfce4-session > ~/.xsession
 sudo cp /home/ubuntu/.xsession /etc/skel
-\cp -f xrdp.ini /etc/xrdp/xrdp.ini
+\cp -f xrdp2.ini /etc/xrdp/xrdp.ini
 sudo service xrdp restart
 sudo apt-get install -y vnc4server
-
+cp xstartup ~/.vnc/xstartup
