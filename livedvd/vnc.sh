f7910d850d953316d2e7b2a9fbb14f37978f4bb7
RuoAndo <ando.ruo@gmail.com>
2016-12-05 18:42:52 +0000

    vnc

--- livedvd/vnc.sh
+++ livedvd/vnc.sh
@@ -3,8 +3,20 @@ sudo -E apt-get update
 sudo -E apt-get install -y ubuntu-desktop
 sudo apt-get install -y xfce4 xrdp
 sudo apt-get install -y xfce4 xfce4-goodies
+#cd
+#mkdir src
+#cd src
+#wget https://github.com/neutrinolabs/xrdp/archive/devel.zip
+#unzip devel.zip
+#cd xrdp-devel
+#sudo apt-get install -y autoconf libtool libssl-dev libpam0g-dev libx11-dev libxfixes-dev libxrandr-dev
+#./bootstrap
+#./configure
+#make
+#sudo make install
 echo xfce4-session > ~/.xsession
 sudo cp /home/ubuntu/.xsession /etc/skel
 \cp -f xrdp.ini /etc/xrdp/xrdp.ini
-sudo apt-get install -y vnc4server
 sudo service xrdp restart
+sudo apt-get install -y vnc4server
+
