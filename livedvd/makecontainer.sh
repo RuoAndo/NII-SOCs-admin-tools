368d5882a174ab019c66122e0396407b4e17eec7
RuoAndo <ando.ruo@gmail.com>
2016-12-05 16:40:17 +0000

    delete

--- livedvd/makecontainer.sh
+++
@@ -1,6 +0,0 @@
-apt install -y live-boot live-boot-initramfs-tools
-apt install -y grub2-common
-apt install -y linux-image-$(uname -r)
-apt install -y network-manager openssh-server openssh-client byobu emacs less lvm2 e2fsprogs net-tools
-apt install -y  gnome-shell-extensions gnome ubuntu-desktop xinit
-sudo systemctl enable ssh
