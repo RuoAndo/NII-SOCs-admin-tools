02efaee1d2e37cf1560b2b75746595f9f8c45af8
RuoAndo <ando.ruo@gmail.com>
2016-12-05 21:00:28 +0000

    make image

--- livedvd/makeimage.sh
+++ livedvd/makeimage.sh
@@ -1,5 +1,9 @@
+cd live-build
+rm -rf iso
+#rm -rf iso/live
 mkdir iso
 mkdir iso/live
 sudo cp -a chroot/boot iso/
 sudo mksquashfs chroot iso/live/filesystem.squashfs
+rm -rf ubuntu-live.iso
 sudo grub-mkrescue -o ubuntu-live.iso iso
