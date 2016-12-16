b1adb297b04165096f96301ffd772004d0a6e8f7
RuoAndo <ando.ruo@gmail.com>
2016-12-16 21:16:48 +0900

    rootfs

--- livedvd/makerootfs.sh
+++ livedvd/makerootfs.sh
@@ -1,4 +1,6 @@
+apt-get update
 sudo apt install -y livecd-rootfs systemd-container xorriso
+rm -rf live-build
 mkdir live-build
 cd live-build/
 cp -a /usr/share/livecd-rootfs/live-build/auto .
@@ -10,8 +12,8 @@ sudo lb build
 sudo cp /etc/apt/sources.list chroot/etc/apt/
 echo "ubuntu-live" | sudo tee chroot/etc/hostname
 echo "127.0.0.1 ubuntu-live" | sudo tee chroot/etc/hosts
-cp ~/nii-cyber-security-admin/livedvd/makecontainer.sh /home/ubuntu/nii-cyber-security-admin/livedvd/live-build/chroot/root/
-cp ~/nii-cyber-security-admin/livedvd/grub.com /home/ubuntu/nii-cyber-security-admin/livedvd/live-build/chroot/root/
+cp ~/nii-cyber-security-admin/livedvd/makecontainer.sh ~/nii-cyber-security-admin/livedvd/live-build/chroot/root/
+cp ~/nii-cyber-security-admin/livedvd/grub.com ~/nii-cyber-security-admin/livedvd/live-build/chroot/root/
 sudo chroot chroot
 passwd
 sudo systemd-nspawn -b -D chroot
