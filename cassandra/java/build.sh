f024fe792e26fd4ee083e6a07ec99fb805a25aa7
root <root@ip-10-0-1-45.ap-northeast-1.compute.internal>
2016-12-22 03:42:42 -0500

    build

--- cassandra/java/build.sh
+++ cassandra/java/build.sh
@@ -1,2 +1,3 @@
-java -cp /root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:./ CassandraConnector
+javac -cp /root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:./ $1.java
+java -cp /root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:./ $1
 
