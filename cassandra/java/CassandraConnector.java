567801936b89b3ecb892836feac53f887761d7fb
root <root@ip-10-0-1-45.ap-northeast-1.compute.internal>
2016-12-22 03:03:02 -0500

    cassandra driver

---
+++ cassandra/java/CassandraConnector.java
@@ -0,0 +1,51 @@
+// package com.marxmart.persistence;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.Session;
+import static java.lang.System.out;
+/**
+ * Class used for connecting to Cassandra database.
+ */
+public class CassandraConnector
+{
+    /** Cassandra Cluster. */
+    private Cluster cluster;
+    /** Cassandra Session. */
+    private Session session;
+    /**
+     * Connect to Cassandra Cluster specified by provided node IP
+     * address and port number.
+     *
+     * @param node Cluster node IP address.
+     * @param port Port of cluster host.
+     */
+    // public void connect(final String node, final int port)
+    public void main(final String node, final int port)
+    {
+	this.cluster = Cluster.builder().addContactPoint(node).withPort(port).build();
+	final Metadata metadata = cluster.getMetadata();
+	out.printf("Connected to cluster: %s\n", metadata.getClusterName());
+	for (final Host host : metadata.getAllHosts())
+	    {
+		out.printf("Datacenter: %s; Host: %s; Rack: %s\n",
+			   host.getDatacenter(), host.getAddress(), host.getRack());
+	    }
+	session = cluster.connect();
+    }
+    /**
+     * Provide my Session.
+     *
+     * @return My session.
+     */
+    public Session getSession()
+    {
+	return this.session;
+    }
+    /** Close cluster. */
+    public void close()
+    {
+	cluster.close();
+    }
+}
