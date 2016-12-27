b8234c58f0322ad2e5008a4085032540dd199da3
root <root@ip-10-0-1-45.ap-northeast-1.compute.internal>
2016-12-26 19:41:32 -0500

    cassandra driver

--- cassandra/java/CassandraConnector.java
+++ cassandra/java/CassandraConnector.java
@@ -1,27 +1,16 @@
-// package com.marxmart.persistence;
-
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Host;
 import com.datastax.driver.core.Metadata;
 import com.datastax.driver.core.Session;
 import static java.lang.System.out;
-/**
- * Class used for connecting to Cassandra database.
- */
+
 public class CassandraConnector
 {
     /** Cassandra Cluster. */
     private Cluster cluster;
     /** Cassandra Session. */
     private Session session;
-    /**
-     * Connect to Cassandra Cluster specified by provided node IP
-     * address and port number.
-     *
-     * @param node Cluster node IP address.
-     * @param port Port of cluster host.
-     */
-    // public void connect(final String node, final int port)
+
     public void main(final String node, final int port)
     {
 	this.cluster = Cluster.builder().addContactPoint(node).withPort(port).build();
@@ -34,16 +23,12 @@ public class CassandraConnector
 	    }
 	session = cluster.connect();
     }
-    /**
-     * Provide my Session.
-     *
-     * @return My session.
-     */
+
     public Session getSession()
     {
 	return this.session;
     }
-    /** Close cluster. */
+
     public void close()
     {
 	cluster.close();
