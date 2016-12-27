0d28c39f3595f6e6317da720b23468cadb0a2515
root <root@ip-10-0-1-45.ap-northeast-1.compute.internal>
2016-12-26 19:47:25 -0500

    cassandra build

--- cassandra/java/build.sh
+++ cassandra/java/build.sh
@@ -1,3 +1,4 @@
-javac -cp /root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:./ $1.java
-java -cp /root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:./ $1
+javac -cp /root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:/root/slf4j-log4j12-1.7.22.jar:/usr/share/cassandra/lib/log4j-1.2.16.jar $1.java
+
+java -cp /root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:/root/guava-19.0.jar:./:/root/slf4j-log4j12-1.7.22.jar:/usr/share/cassandra/lib/slf4j-api-1.7.2.jar:/usr/share/cassandra/lib/slf4j-log4j12-1.7.2.jar:/usr/share/cassandra/lib/log4j-1.2.16.jar:/root/netty-4.1.6.Final.jar $1
 
