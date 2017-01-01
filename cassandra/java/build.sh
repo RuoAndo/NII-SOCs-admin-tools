e7d84c3a690c8b47916d73536b18a8dc27f4bf6d
RuoAndo <ando.ruo@gmail.com>
2017-01-01 10:49:40 +0000

    build

--- cassandra/java/build.sh
+++ cassandra/java/build.sh
@@ -1,4 +1,11 @@
-javac -cp /root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:/root/slf4j-log4j12-1.7.22.jar:/usr/share/cassandra/lib/log4j-1.2.16.jar $1.java
+#javac -cp /root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:/root/slf4j-log4j12-1.7.22.jar:/usr/share/cassandra/lib/log4j-1.2.16.jar $1.java
 
-java -cp /root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:/root/guava-19.0.jar:./:/root/slf4j-log4j12-1.7.22.jar:/usr/share/cassandra/lib/slf4j-api-1.7.2.jar:/usr/share/cassandra/lib/slf4j-log4j12-1.7.2.jar:/usr/share/cassandra/lib/log4j-1.2.16.jar:/root/netty-4.1.6.Final.jar $1
+rm -rf *.class guava-20.0.jar
+
+
+wget https://github.com/google/guava/releases/download/v20.0/guava-20.0.jar
+
+javac -cp .:./guava-20.0.jar:/root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:/root/slf4j-log4j12-1.7.22.jar:/usr/share/cassandra/lib/log4j-1.2.16.jar $1.java
+
+java -cp .:./guava-20.0.jar:/root/.m2/repository/com/datastax/cassandra/cassandra-driver-core/3.0.3/cassandra-driver-core-3.0.3.jar:/root/java-driver/driver-dist/target/cassandra-java-driver-3.2.0-SNAPSHOT.jar:/root/guava-19.0.jar:./:/root/slf4j-log4j12-1.7.22.jar:/usr/share/cassandra/lib/slf4j-api-1.7.2.jar:/usr/share/cassandra/lib/slf4j-log4j12-1.7.2.jar:/usr/share/cassandra/lib/log4j-1.2.16.jar:/root/netty-4.1.6.Final.jar $1
 
