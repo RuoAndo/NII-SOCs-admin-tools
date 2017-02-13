82e6f7a4f5909aef0787601ad4c2bf09193ced3f
Ruo Ando <ando.ruo@gmail.com>
2017-02-13 11:00:12 +0900

    kernel

---
+++ detection/SVM/README.md
@@ -0,0 +1,8 @@
+linear
+<img src="linear.png">
+
+sigmoid
+<img src="sigmoid.png">
+
+rbf
+<img src="rbf.png">
---
+++ detection/SVM/sigmoid.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.svm import SVC
+from matplotlib.colors import ListedColormap
+
+
+def versiontuple(v):
+    return tuple(map(int, (v.split("."))))
+
+
+def plot(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup frame
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+
+    # classifier = SVM
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+    if test_idx:
+        
+        if not versiontuple(np.__version__) >= versiontuple('1.9.0'):
+            X_test, y_test = X[list(test_idx), :], y[list(test_idx)]
+            warnings.warn('Please update to NumPy 1.9.0 or newer')
+        else:
+            X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    alpha=1.0,
+                    linewidths=1,
+                    marker='o',
+                    s=55, label='test set')
+
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0,
+                       X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+
+plt.scatter(X_xor[y_xor == 1, 0],
+            X_xor[y_xor == 1, 1],
+            c='b', marker='x',
+            label='1')
+plt.scatter(X_xor[y_xor == -1, 0],
+            X_xor[y_xor == -1, 1],
+            c='r',
+            marker='s',
+            label='-1')
+
+svm = SVC(kernel='sigmoid', random_state=0, gamma=0.10, C=10.0)
+svm.fit(X_xor, y_xor)
+plot(X_xor, y_xor, classifier=svm)
+
+plt.legend(loc='upper left')
+plt.tight_layout()
+# plt.savefig('./figures/support_vector_machine_rbf_xor.png', dpi=300)
+plt.show()
