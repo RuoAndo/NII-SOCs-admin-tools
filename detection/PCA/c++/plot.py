034ec49ff538ba9eacbb353e9ef387dd5ac0fd11
Ruo Ando <ando.ruo@gmail.com>
2017-03-21 23:44:41 +0900

    PCM

---
+++ detection/PCA/c++/plot.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+ptn = re.compile(r'\s+')
+
+from pylab import *
+f = open("tmp", "r")
+xlist = list()
+ylist = list()
+for line in f:
+    line2 = line.lstrip()
+    #s = line2.split(" ")
+    s = ptn.split(line2)
+    print s
+    xx = s[0]
+    yy = s[1][:-1]
+    xlist.append(xx)
+    ylist.append(yy)
+f.close()
+
+plt.scatter(xlist,ylist)
+plt.show()
+
+
+
