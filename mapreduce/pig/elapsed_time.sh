5ef725d3a0a213ef1bca18355d39a8e1c65d590f
Ruo Ando <ando.ruo@gmail.com>
2017-04-17 11:37:38 +0900

    pig

--- mapreduce/pig/elapsed_time.sh
+++ mapreduce/pig/elapsed_time.sh
@@ -1,1 +1,2 @@
-pig -x local -param SRCS=$1 -param IP=$2 -param DUMP=$3 elapsed_time.pig
+/root/pig-0.16.0/bin/pig -param SRCS=$1 -param IP=$2 -param DUMP=$3 elapsed_time.pig
+#pig -x local -param SRCS=$1 -param IP=$2 -param DUMP=$3 elapsed_time.pig
