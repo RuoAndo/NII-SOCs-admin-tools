e3011e42dee1b5014df766b85a25fdcd9759af24
Ruo Ando <ando.ruo@gmail.com>
2017-05-29 12:33:30 +0900

    myudfs

---
+++ mapreduce/pig/udf/myudfs/UPPER.java
@@ -0,0 +1,18 @@
+package myudfs;
+import java.io.IOException;
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+
+public class UPPER extends EvalFunc<String>
+{
+    public String exec(Tuple input) throws IOException {
+        if (input == null || input.size() == 0)
+            return null;
+        try{
+            String str = (String)input.get(0);
+           return str.toUpperCase();
+        }catch(Exception e){
+            throw new IOException("Caught exception processing input row ", e);
+        }
+    }
+}
