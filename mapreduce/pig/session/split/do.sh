28eeda35668b8574e8f4b32d3512a77d8add5228
Ruo Ando <ando.ruo@gmail.com>
2017-06-13 21:44:13 +0900

    do.sh

--- mapreduce/pig/session/split/do.sh
+++ mapreduce/pig/session/split/do.sh
@@ -1,5 +1,11 @@
-./addrpair.sh $1
-./addrpair-join.sh $1
-./addrpair-sid.sh $1
-./addrpair-avg.sh $1
-./join.sh $1
+TESTFILE=$1
+while read line; do
+    echo $line
+    hadoop fs -put $line
+    ./addrpair.sh $line
+    ./addrpair-join.sh $line
+    ./addrpair-sid.sh $line
+    ./addrpair-avg.sh $line
+    ./join.sh $line
+done < $1
+
