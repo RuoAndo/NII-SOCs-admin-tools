2669b83c7d2c54405a081bf4f3dda3d47ee2f9c7
Ruo Ando <ando.ruo@gmail.com>
2017-06-13 22:30:26 +0900

    join

--- mapreduce/pig/session/split/join.sh
+++ mapreduce/pig/session/split/join.sh
@@ -1,4 +1,4 @@
 rm -rf addpair-join-$1
 hadoop fs -rmr addpair-join-$1
 pig -param DIRAVG=addrpair-avg-$1 -param DIRSID=addrpair-sid-$1 -param OUTPUTDIR=addrpair-join-$1 join.pig
-hadoop fs -get addpair-join-$1 
+hadoop fs -get addrpair-join-$1 
