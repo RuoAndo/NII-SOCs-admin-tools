e9ea0553946c05034f98c07b136b6cfd35f4a474
Ruo Ando <ando.ruo@gmail.com>
2017-07-13 10:52:36 +0900

    makelist

---
+++ mapreduce/pig/session/split/do-makelist.sh
@@ -0,0 +1,8 @@
+\cp ../piggybank.jar .
+\cp ../addr.jar .
+\cp ../date.jar .
+./do.sh list
+./cat.sh list
+./makelist.sh
+#./init.sh
+#./repeat.sh
---
+++ mapreduce/pig/session/split/makelist.sh
@@ -0,0 +1,9 @@
+echo "label.sh"
+./label.sh addrpair-join-all
+echo "cat-cls.sh"
+./cat-cls.sh
+echo "cat-avg.sh"
+./cat-avg.sh
+#echo "4.py"
+#python 4.py cat-avg-all cat-cls-all > r
+#python time python 1.py r
