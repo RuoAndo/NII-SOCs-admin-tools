304b78b65d03fe0dc33be6c5b0525a90845b5053
Ruo Ando <ando.ruo@gmail.com>
2017-07-19 12:34:19 +0900

    auto

--- mapreduce/pig/session/split/do-distance.sh
+++ mapreduce/pig/session/split/do-distance.sh
@@ -1,10 +1,14 @@
 g++ distance3.cpp
 
+rm -rf clstmp
+touch clstmp
+
 while read line; do
     wc=`wc -l $line`
     wc2=`echo $wc | cut -d " " -f 1`
     echo $wc2
     python trans.py $line > $line-trans
-    ./a.out cat-avg-all 5 4 $line-trans $wc2 6
+    echo "processing $line-trans.."
+    ./a.out cat-avg-all 5 4 $line-trans $wc2 6 >> clstmp
 done < $1
 
