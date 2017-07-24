730a934035a773f3396052f80241b3b8b2b0fa55
Ruo Ando <ando.ruo@gmail.com>
2017-07-24 21:47:08 +0900

    auto

--- mapreduce/pig/session/split2/init.sh
+++ mapreduce/pig/session/split2/init.sh
@@ -5,6 +5,6 @@ echo "cat-cls.sh"
 ./cat-cls.sh
 echo "cat-avg.sh"
 ./cat-avg.sh
-#echo "4.py"
-#time python three.py cat-avg-all cat-cls-all > r
+echo "three.py"
+time python three.py cat-avg-all cat-cls-all > r
 #time python 1.py r
