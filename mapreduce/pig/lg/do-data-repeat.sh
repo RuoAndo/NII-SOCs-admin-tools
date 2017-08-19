d5a7a6ce4e04148f23d6e32e01df1f6dfea2c3fe
Ruo Ando <ando.ruo@gmail.com>
2017-08-19 16:56:45 +0900

    repeat hdfs

---
+++ mapreduce/pig/lg/do-data-repeat.sh
@@ -0,0 +1,14 @@
+for line in `cat ${1}`
+do
+    echo $line
+    hadoop fs -rmr $line
+    hadoop fs -put $line
+    ./addrpair.sh $line
+    ./addrpair-join.sh $line
+    ./addrpair-sid.sh $line
+    ./addrpair-avg.sh $line
+    ./join.sh $line
+    ./restart.sh
+done
+
+
