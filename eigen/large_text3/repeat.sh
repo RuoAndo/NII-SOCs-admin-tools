911d900d47f188b433093460b6cb4a83a3e0f122
Ruo Ando <ando.ruo@gmail.com>
2017-09-03 02:07:03 +0900

    repeat

--- eigen/large_text3/repeat.sh
+++ eigen/large_text3/repeat.sh
@@ -1,24 +1,13 @@
-\cp c2 c2.bak
-
-#python fill5.py c2 | tee c3
-#python fill7.py c2 | tee c3
-
-nl=`wc -l c2 | cut -d ' ' -f1` 
-echo "LN:" $nl
-./core c2 $nl 5 500000 5
+./avg2.sh all2 # generates avg-all2
+nl=`wc -l avg-all2 | cut -d ' ' -f1` 
+./core avg-all2 $nl 4 500000 5
 ./cat.sh list2 # generates "all2"
-python 0.py all2 | tee nclstr
-./avg2.sh all2
-\cp avg-all2 c2
+python 0.py all2
+python concate.py all2 avg-all2 | tee result
 
-python comp.py c2.bak c2
+echo "current"
+cat result
+echo "previous"
+cat result.bak
 
-<<<<<<< HEAD
-#python concate2.py all2 avg-all2 c3 | tee c2
-
-=======
->>>>>>> 1da744fa630923bd6090e89eb44dc3c88b9f50a8
-#python file.py c4 c3 > c2
-#python 1.py all2 nclstr.bak 
-#cat SSE
-#\cp nclstr nclstr.bak
+\cp result result.bak
