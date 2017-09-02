e6463cf544557aad2775e235f0827528a2a561b6
Ruo Ando <ando.ruo@gmail.com>
2017-09-02 16:16:16 +0900

    first-conc

--- eigen/large_text3/first.sh
+++ eigen/large_text3/first.sh
@@ -1,17 +1,20 @@
 rm -rf nclstr.bak
 
 python conv.py all > all-conv
-./avg.sh all-conv
+./avg.sh all-conv # generates "avg-all-conv" and "c"
+
 split -l 500000 all-conv out
 ls out* > list
 ./rename.sh list
+
 ./core c 10 4 500000 5
+
 ls *.labeled > list2
 ./cat.sh list2
-python 0.py all2
 
-#./avg2.sh all2
-#python fill2.py c > c2
-#./first c2 10 4 500000 5
-#./cat.sh list2
 #python 0.py all2
+
+python concate.py all2 avg-all-conv > c2                    
+#0,0,4254.970199115338,11538.261469900643,2.557382710801764
+#435164,1,1300.0852150323146,11533.86312135961,2.542900077129711
+
