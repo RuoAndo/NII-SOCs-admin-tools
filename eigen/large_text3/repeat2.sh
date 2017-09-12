9a688cb3e230ca5a86a4a427b996aae9bc687f31
Ruo Ando <ando.ruo@gmail.com>
2017-09-12 22:29:31 +0900

    repeat2

--- eigen/large_text3/repeat2.sh
+++ eigen/large_text3/repeat2.sh
@@ -1,9 +1,15 @@
 \cp c2-sorted c2.bak
 #python fill5.py c2 | tee c3
 python fill7.py c2 | tee c3
+
+echo "processing stage 1 (core)..."
 ./core c3 10 5 500000 5
 ./cat.sh list2 # generates "all2"
+
+echo "processing stage 2 (0.py)..."
 python 0.py all2 | tee nclstr
+
+echo "processing stage 3 (avg)..."
 ./avg2.sh all2
 python concate2.py all2 avg-all2 c3 | tee c2
 ./sort.pl c2 > c2-sorted
