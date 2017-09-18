1da4b23e6203ecb76d26441c6ccaef9b5c290d7a
Ruo Ando <ando.ruo@gmail.com>
2017-09-18 17:51:03 +0900

    first

--- eigen/large_text4/first.sh
+++ eigen/large_text4/first.sh
@@ -2,9 +2,10 @@
 ./build.sh avg
 ./build.sh relabel
 
-split -l 500000 all out
+split -l 300000 $1 out
 ls out* > list
-head -n 20 list > list-20
-./rename.sh list-20
+#head -n 20 list > list-20
+#./rename.sh list-20
+time ./rename.sh list
 echo "now initlializing labels ..."
 time ./rand-labeling 500000 5
