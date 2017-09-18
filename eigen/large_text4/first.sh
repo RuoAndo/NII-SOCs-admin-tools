d81fe99964c9f07f687f99a0fd1203c7fc97d806
Ruo Ando <ando.ruo@gmail.com>
2017-09-18 18:49:40 +0900

    auto

--- eigen/large_text4/first.sh
+++ eigen/large_text4/first.sh
@@ -1,11 +1,14 @@
+LN=300000
+NC=5
+
 ./build.sh rand-labeling
 ./build.sh avg
 ./build.sh relabel
 
-split -l 300000 $1 out
+split -l $LN $1 out
 ls out* > list
 #head -n 20 list > list-20
 #./rename.sh list-20
 time ./rename.sh list
 echo "now initlializing labels ..."
-time ./rand-labeling 500000 5
+time ./rand-labeling $LN $NC
--- eigen/large_text4/second.sh
+++ eigen/large_text4/second.sh
@@ -1,27 +1,28 @@
-CLUSTER_NO=5
+NC=5
+LN=30000
 
 rm -rf *.relabeled
 
-echo "concatenating ..."
+echo "STEP1: concatenating ..."
 ls *.labeled > list-labeled
 ./sort.pl list-labeled > list-labeled-sorted
 
-head -n 20 list-labeled-sorted > list-labeled-20
-./cat-labeled.sh list-labeled-20 #test # yields all-labeled
+#head -n 20 list-labeled-sorted > list-labeled-20
+#./cat-labeled.sh list-labeled-20 #test # yields all-labeled
 
-#./cat-labeled.sh list-labeled # yields all-labeled
+./cat-labeled.sh list-labeled-sorted # yields all-labeled
 
-echo "counting points per cluster..."
+echo "STEP2: counting points per cluster..."
 time python 0.py all-labeled | tee tmp-all-labeled #test
 #time python 0.py all-labeled | tee tmp-all-labeled
 sleep 2s
 
-echo "calculating centroid..."
-time ./avg 500000 5 # on *.labled # test
+echo "STEP3: calculating centroid..."
+time ./avg $LN $NC # on *.labled # test
 #time ./avg 500000 6 # on *.labled
 sleep 2s
 
-echo "filling blank centroid..."
+echo "STEP4: filling blank centroid..."
 #python fill.py tmp-all-labeled centroid > tmp-centroid
 python fill2.py tmp-all-labeled centroid > tmp-centroid
 cat tmp-centroid
@@ -29,25 +30,25 @@ cat tmp-centroid
 sleep 4s
 
 # relabel
-time ./relabel centroid $CLUSTER_NO 3 500000 6 # on *.labeled to *.relabeled
+time ./relabel centroid $NC 3 $LN $NC # on *.labeled to *.relabeled
 
 # concatenate
 time ls *.relabeled > list-relabeled
 ./sort.pl list-relabeled > list-relabeled-sorted 
-head -n 20 list-relabeled-sorted > list-relabeled-20 #test
-time ./cat-relabeled.sh list-relabeled-20 # test
+#head -n 20 list-relabeled-sorted > list-relabeled-20 #test
+#time ./cat-relabeled.sh list-relabeled-20 # test
 
-#time ./cat-relabeled.sh list-relabeled 
+time ./cat-relabeled.sh list-relabeled-sorted
 
-echo "counting points per cluster..."
+echo "STEP5: counting points per cluster..."
 time python 0.py all-relabeled | tee tmp-all-relabeled
 sleep 2s
 
-echo "converting *.labeled to *.relabeled..."
-time ./rename2.sh list-relabeled-20 # test
-#time ./rename2.sh list-relabeled
+echo "STEP6: converting *.labeled to *.relabeled..."
+#time ./rename2.sh list-relabeled-20 # test
+time ./rename2.sh list-relabeled-sorted
 
-echo "calculating SSE..."
+echo "STEP7: calculating SSE..."
 time python sse.py centroid tmp-all-relabeled tmp-all-labeled
 #cat SSE
 sleep 2s
