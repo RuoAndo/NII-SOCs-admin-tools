a2fdaf2f7ccdbf30753c1fac762349c60114c37e
RuoAndo <ando.ruo@gmail.com>
2017-09-29 16:06:08 +0900

    auto

--- detection/CD1/do.sh
+++ detection/CD1/do.sh
@@ -1,4 +1,51 @@
-python cut.py $1 > $1-cut
-./main $1-cut > $1-out
-python diff1.py $1-out > $1-diff
-python std.py $1-diff $1 instlist > $1-std
+rm -rf in_*
+rm -rf out_*
+
+while read line; do
+    echo $line
+    cd $line
+
+    cp -r ../gendata . 
+
+    rm -rf in_*
+    rm -rf out_*
+    
+    ./trans.sh 
+    ./gen-data2.sh # yields *_in and *_out
+    
+    ls in_* > inlist
+    while read line2; do
+	echo $line2
+	echo $line2_$line
+	cp $line2 ${line2}_${line}
+	cp ${line2}_${line} ../
+    done < inlist
+
+    ls out_* > outlist
+    while read line2; do
+	echo $line2
+	echo $line2_$line
+	cp $line2 ${line2}_${line}
+	cp ${line2}_${line} ../
+    done < outlist
+    
+done < $1
+
+cd ..
+
+while read line; do
+    echo "ID: " ${line}
+    
+    ls in_* | grep ${line} > inlist_${line}
+    rm -rf ${line}-all
+    touch ${line}-all
+
+    # in_10034_20170920
+    
+    while read line2; do
+	echo "cat " ${line2}
+	cat ${line2} >> ${line}-all
+    done < inlist_${line}
+	
+done < $2
+    
