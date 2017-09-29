776fbedbfb4df4c6adddd70cfbca746113e171de
Ruo Ando <ando.ruo@gmail.com>
2017-09-29 20:22:14 +0900

    2000

--- eigen/vldc_driver/vldc_data/vldc_data.h
+++ eigen/vldc_driver/vldc_data/vldc_data.h
@@ -45,11 +45,11 @@
 #endif
 
 #ifndef VLDC_DATA_NR_DEVS
-#define VLDC_DATA_NR_DEVS 20    /* vldc_data0 through vldc_data3 */
+#define VLDC_DATA_NR_DEVS 2000    /* vldc_data0 through vldc_data3 */
 #endif
 
 #ifndef VLDC_DATA_P_NR_DEVS
-#define VLDC_DATA_P_NR_DEVS 20  /* vldc_datapipe0 through vldc_datapipe3 */
+#define VLDC_DATA_P_NR_DEVS 2000  /* vldc_datapipe0 through vldc_datapipe3 */
 #endif
 
 /*
