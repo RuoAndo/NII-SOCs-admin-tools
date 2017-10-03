c62e365b9559e3dbaf248ae906932531b9bc017e
Ruo Ando <ando.ruo@gmail.com>
2017-10-03 12:39:12 +0900

    auto

--- mapreduce/pig/session/reduce/do.sh
+++ mapreduce/pig/session/reduce/do.sh
@@ -1,4 +1,12 @@
-#time ./traverse2/traverse2 133.52.62.12 test > tmp
+if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" == "" ];
+then
+    echo "argument required"
+fi
+
+
+cd ./traverse2
+make
+cd ..
 time ./traverse2/traverse2 $1 $2 > tmp
 
 time ./reduce.sh tmp
@@ -6,3 +14,15 @@ time pig reduce2.pig > tmp2
 
 python clean.py tmp2 > tmp3
 ./drem.pl tmp3 > tmp4
+
+#cat tmp4
+python dir.py tmp4 $1 > tmp5
+python cut.py tmp4 $1 > tmp6
+
+python dir.py tmp4 $1 > reduce-$3-$1-$4-`date +%Y%m%d_%H-%M-%S`
+\cp reduce-$3-$1-$4-`date +%Y%m%d_%H-%M-%S` /data1/reduced-data/
+
+python cut.py tmp4 $1 > reduce-$3-$1-$4-list-`date +%Y%m%d_%H-%M-%S`
+\cp reduce-$3-$1-$4-list-`date +%Y%m%d_%H-%M-%S` /data1/reduced-data/
+
+#./slmbr.sh 1 > tmp-s
