6a9f8c8921c40ff1aa67929bedbdddf0779ef39a
Ruo Ando <ando.ruo@gmail.com>
2017-10-04 18:13:04 +0900

    auto

---
+++ detection/CD-pa/auto.sh
@@ -0,0 +1,16 @@
+if [ "$1" = "" ]
+then
+    echo "no argument: ./auto.sh yyyy mm dd"
+fi
+
+ls *csv > csvlist
+./sort-csv.pl csvlist> csvlist-sorted
+python gen-wList.py csvlist-sorted > warnlist
+rm -rf fp-*; python 3.py warnlist csvlist-sorted > tmp
+ls fp-* > fplist
+time ./do.sh fplist
+ls rnn_* > rnnlist
+./cat.sh rnnlist
+./sort-rnn.pl rnn-all > rnn-all-sorted
+python addWarn.py warnlist rnn-all-sorted > rnn-all-sorted-warned
+python date-trans.py rnn-all-sorted-warned 2017 09 20 > rnn-all-sorted-warned-dated
