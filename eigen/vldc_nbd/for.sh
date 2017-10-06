a3d58d6dbc5a37eeaeb4f1c4eec760ca0e69bd3d
Ruo Ando <ando.ruo@gmail.com>
2017-10-06 22:59:39 +0900

    for

---
+++ eigen/vldc_nbd/for.sh
@@ -0,0 +1,11 @@
+a=0
+while [ $a -ne 900 ]
+do
+    echo "nbd"$a
+    ./busexmp /dev/nbd$a &
+    mkfs.ext4 /dev/nbd$a
+    rm -rf /mnt/nbd$a
+    mkdir /mnt/nbd$a
+    mount /dev/nbd$a /mnt/nbd$a
+    a=`expr $a + 1`
+done
