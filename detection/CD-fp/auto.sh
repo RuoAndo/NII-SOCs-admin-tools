41571006a20e9dee8f1475d719ac040dd3b8dbcc
Ruo Ando <ando.ruo@gmail.com>
2017-10-12 21:23:26 +0900

    auto

--- detection/CD-fp/auto.sh
+++ detection/CD-fp/auto.sh
@@ -15,8 +15,11 @@ fi
 ./listup.pl | grep HIBETSU > csvlist
 python trans-sort.py csvlist 
 ls *csv > csvlist
+echo "sorting csvlist..."  
 ./sort-csvlist.pl csvlist > csvlist-sorted
+echo "sorting warnlist..."  
 python gen-wList.py csvlist-sorted > warnlist
+echo "generating fp* files..."  
 rm -rf fp-*; python 3.py warnlist csvlist-sorted > tmp
 ls fp-* > fplist
 time ./do.sh fplist
