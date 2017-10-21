b649791185d4fef5e0fa892a2cad2162ca68dc85
Ruo Ando <ando.ruo@gmail.com>
2017-10-21 15:09:44 +0900

    10

--- detection/CD-fp/10.py
+++ detection/CD-fp/10.py
@@ -1,15 +1,22 @@
 import sys 
 import os
+import commands
 
 argvs = sys.argv  
 argc = len(argvs) 
+
 
 f = open(argvs[1])
 line = f.readline() 
 
 while line:
     tmp = line.split(",")
-    print tmp[1]
+    #print(tmp[1])
+    
+    comstr = "grep -n " + "\"" + tmp[1] + "\"" + " " + argvs[2]
+    #print(comstr)
+    check = commands.getoutput(comstr)    
+    print check + "," + tmp[2].strip()
     line = f.readline() 
 
 f.close()
