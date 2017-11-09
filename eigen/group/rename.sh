acf6e28c19392625da3f460de394ce2951da5459
Ruo Ando <ando.ruo@gmail.com>
2017-11-09 18:08:21 +0900

    rename

--- eigen/group/rename.sh
+++ eigen/group/rename.sh
@@ -2,6 +2,7 @@ COUNTER=0
 for line in `cat ${1}`
 do
     echo "now processing " $line ":" $COUNTER " ..."
-    python trans.py $line > $COUNTER
+    #python trans.py $line > $COUNTER
+    \cp $line > $COUNTER
     COUNTER=`expr $COUNTER + 1`
 done
