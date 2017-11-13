09b2d42dfe1ddf292dcc293d69b54489ad88b1c6
Ruo Ando <ando.ruo@gmail.com>
2017-11-13 16:24:38 +0900

    kf-auto

--- detection/CD-session-KF/auto.sh
+++ detection/CD-session-KF/auto.sh
@@ -1,32 +1,29 @@
-rm -rf rnn_*
+rm -rf kf_*
 
-rm -rf rnn_all_in
-touch rnn_all_in
+rm -rf kf_all_in
+touch kf_all_in
 
-rm -rf rnn_all_out
-touch rnn_all_out
+rm -rf kf_all_out
+touch kf_all_out
 
 while read line; do
     echo $line
-    rm -rf rnn_in_${line}
+    rm -rf kf_in_${line}
     python kf2.py in_${line}_all instlist
-    cat rnn_in_${line} >> rnn_all_in
+    cat kf_in_${line} >> kf_all_in
 done < $1
 
-./sort.pl rnn_all_in > tmp_in
+./sort.pl kf_all_in > tmp_in
 
 # output: timeslot, score,     ID,   name
 #              17, 0.00717402,XXXX, YYYY
 
 while read line; do
     echo $line
-    rm -rf rnn_out_${line}
+    rm -rf kf_out_${line}
     python kf2.py out_${line}_all instlist
-    cat rnn_out_${line} >> rnn_all_out
+    cat kf_out_${line} >> kf_all_out
 done < $1
 
-./sort.pl rnn_all_out > tmp_out
-
-
-
+./sort.pl kf_all_out > tmp_out
 
