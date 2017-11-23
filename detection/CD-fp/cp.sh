9c5136d31e53baa7a85fde46284051ea9ed07c64
Ruo Ando <ando.ruo@gmail.com>
2017-11-23 20:12:44 +0900

    cp

--- detection/CD-fp/cp.sh
+++ detection/CD-fp/cp.sh
@@ -1,4 +1,4 @@
-ls -L1 /data1/count_Alarm_Each/FirePOWER | grep 2017 | grep -v csv
+ls -L1 /data1/count_Alarm_Each/FirePOWER | grep 2017 | grep -v csv > list
 
 while read line; do
     echo "copying "$line
