6bc2eb99b5333e6835e25e7f5ded368a11e0ff3a
Ruo Ando <ando.ruo@gmail.com>
2017-11-29 12:52:14 +0900

    cat

--- eigen/group/scripts-1_1000/cat-reduced.sh
+++ eigen/group/scripts-1_1000/cat-reduced.sh
@@ -1,13 +1,13 @@
-today=$(date "+%Y%m%d")
-
+#date=$(date "+%Y%m%d")
+date=`date --date '2 day ago' +%Y%m%d`
 find . -maxdepth 1 -type d | grep dir > dirlist
 
-rm -rf reduced_all-$today
-touch reduced_all-$today
+rm -rf reduced_all-$date
+touch reduced_all-$date
 
 while read line; do
     echo $line
     #cd $Line
     #wc -l $line/reduced
-    cat $line/reduced >> reduced_all-$today
+    cat $line/reduced >> reduced_all-$date
 done < dirlist
