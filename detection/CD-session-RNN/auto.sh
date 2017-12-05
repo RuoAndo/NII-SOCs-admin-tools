39c09809a57b12161d14f07388034f7d2460535b
Ruo Ando <ando.ruo@gmail.com>
2017-12-05 13:51:20 +0900

    auto

--- detection/CD-session-RNN/auto.sh
+++ detection/CD-session-RNN/auto.sh
@@ -17,7 +17,8 @@ while read line; do
     echo $line
     rm -rf rnn_in_${line}
     python 20.py in_${line}_all instlist
-    cat rnn_in_${line} >> rnn_all_in
+    #cat rnn_in_${line} >> rnn_all_in
+    cat rnn_${line} >> rnn_all_in
 done < $1
 
 ./sort.pl rnn_all_in > tmp_in
@@ -26,7 +27,8 @@ while read line; do
     echo $line
     rm -rf rnn_out_${line}
     python 20.py out_${line}_all instlist
-    cat rnn_out_${line} >> rnn_all_out
+    #cat rnn_out_${line} >> rnn_all_out
+    cat rnn_${line} >> rnn_all_out
 done < $1
 
 ./sort.pl rnn_all_out > tmp_out
