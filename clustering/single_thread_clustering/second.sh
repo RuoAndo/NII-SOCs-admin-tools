231596d10fb3d27a606059a06cb8ced2613d711d
Ruo Ando <ando.ruo@gmail.com>
2018-01-04 19:05:04 +0900

    second

--- clustering/single_thread_clustering/second.sh
+++ clustering/single_thread_clustering/second.sh
@@ -1,28 +1,29 @@
 # cluster size
 nClusters=40
-nDimensions=6
-nItems=3 # nDimensions-2 / items: src dst n[* * * *] 
+nDim=6
+nItems=4 # nDimensions-2 / items: src dst n[* * * *] 
 nThreads=1
 
 nLines=`wc -l 0 | cut -d " " -f 1` 
+echo $nLines
 echo "counting points per cluster..."
 time ./count $nLines 1 | tee tmp-all-labeled 
 #sleep 2s
 
 echo "calculating centroid..."
 rm -rf centroid
-time ./avg $nLines $nDimensions #yields centroid
+time ./avg $nLines $nDim #yields centroid
 cat centroid
-#sleep 2s
+sleep 2s
 
 echo "filling blank centroid rows..."
-python fill2.py tmp-all-labeled centroid $nLines $nDimensions > tmp-centroid
+python fill2.py tmp-all-labeled centroid $nLines $nDim > tmp-centroid
 cat tmp-centroid
 \cp tmp-centroid centroid
 #sleep 4s
 
 echo "relabeling ..."
-./relabel centroid $nClusters $nItems $nLines $nDimensions 
+./relabel centroid $nClusters $nItems $nLines $nDim 
 
 \cp 0.rlbl 0.lbl
 time ./count $nLines 1 | tee tmp-all-relabeled 
@@ -31,5 +32,5 @@ time ./count $nLines 1 | tee tmp-all-relabeled
 echo "calculating SSE..."
 time python sse.py centroid tmp-all-relabeled tmp-all-labeled
 
-#./build.sh pickup2; ./pickup2 centroid $nClusters $nItems $nLines $nDimensions | tee tmp
+#./build.sh pickup2; ./pickup2 centroid $nClusters $nItems $nLines $nDim | tee tmp
 #python reverse.py tmp
