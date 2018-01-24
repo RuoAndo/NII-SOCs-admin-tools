2fcbc6eca83d8926580b2f04fb92adabaf96c9f2
Ruo Ando <ando.ruo@gmail.com>
2018-01-24 15:21:39 +0900

    repeat

--- clustering/single_thread_clustering/repeat.sh
+++ clustering/single_thread_clustering/repeat.sh
@@ -1,21 +1,15 @@
 COUNTER=0
-COUNTER_BAK=0
-a=0
 
-while [ $a -ne 10 ]
+rm -rf tmp-repeat
+touch tmp-repeat
+
+while [ $COUNTER -ne 4 ]
 do
-    time python sse.py diff-COUNTER_BAK diff-COUNTER
-    ls *.labeled > list-labeled
-    ./cat-labeled.sh list-labeled 
-    time python 0.py all-relabeled | tee diff-COUNTER_BAK
-    echo "calculating centroid..."
-    time ./avg 500000 6 # on *.labled
-    time ./relabel centroid 10 3 500000 6 # on *.labeled to *.relabeled
-    time ./cat-relabeled.sh list-relabeled 
-    echo "counting points per cluster..."
-    time python 0.py all-relabeled
-    echo "converting *.labeled to *.relabeled..."
-    time ./rename2.sh list-relabeled | tee diff-COUNTER
-    COUNTER=`expr $COUNTER + 1`
+    time ./first.sh $1
+    time ./second.sh
+    #time ./second.sh
+    #time ./second.sh
+    ./pickup-repeat.sh >> tmp-repeat
     COUNTER_BAK=`expr $COUNTER - 1`
+    COUNTER=`expr $COUNTER + 1`
 done
