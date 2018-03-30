4370f51072673fd1f3e6aac88904a4ff9d12695b
Ruo Ando <ando.ruo@gmail.com>
2018-03-30 16:51:18 +0900

    py

--- clustering/2/rand6.py
+++ clustering/2/rand6.py
@@ -2,63 +2,68 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.random import *
+import sys
+
+argvs = sys.argv
+argc = len(argvs) 
+
+if (argc != 3): 
+    print 'Usage: # python d N for ex, 6 * 1000 / 2 = 3000'
+    quit()    
 
 rng = np.random.RandomState(123)
 
-d = 6
-N = 10
+# 6 * 1000 /2 = 3000 tmp
 
-mean1 = 5
-mean2 = 10
-mean3 = 15
-mean4 = 20
-mean5 = 25
+d = int(argvs[1])
+N = int(argvs[2])
+
+dN = d * N
+
+SRC = rand(dN) * 40 + 30 # 30?70の乱数を100個生成
+DST = rand(dN) * 40 + 30 # 30?70の乱数を100個生成
+
+src = []
+for x in SRC:
+    src.append(int(x))
+
+dst = []
+for x in DST:
+    dst.append(int(x))
+    
+mean = 5
 
 x1 = rng.randn(N, d) + np.array([0,0,0,0,0,0])
-x2 = rng.randn(N, d) + np.array([mean1, mean1, mean1, mean1, mean1, mean1])
-x3 = rng.randn(N, d) + np.array([mean2, mean2, mean2, mean2, mean2, mean2])
-x4 = rng.randn(N, d) + np.array([mean3, mean3, mean3, mean3, mean3, mean3])
-x5 = rng.randn(N, d) + np.array([mean4, mean4, mean4, mean4, mean4, mean4])
-x6 = rng.randn(N, d) + np.array([mean5, mean5, mean5, mean5, mean5, mean5])
+x2 = rng.randn(N, d) + np.array([mean*2, mean, mean*2, mean, mean*2, mean])
+x3 = rng.randn(N, d) + np.array([mean, mean*2, mean, mean*2, mean, mean*2])
 
-x = np.concatenate((x1, x2, x3, x4, x5, x6), axis=0)
+x = np.concatenate((x1, x2, x3), axis=0)
+
+counter = 0
 
 for i in x1:
     constr = ""
     for j in i:
         constr = constr + str(j) + ","
-    print constr.rstrip(",")
-
+    print str(src[counter]) + "," + str(dst[counter]) + "," + constr.rstrip(",")
+    counter = counter + 1
+    
 for i in x2:
     constr = ""
     for j in i:
         constr = constr + str(j) + ","
-    print constr.rstrip(",")
+    #print constr.rstrip(",")
+    print str(src[counter]) + "," + str(dst[counter]) + "," + constr.rstrip(",")
+    counter = counter + 1
 
 for i in x3:
     constr = ""
     for j in i:
         constr = constr + str(j) + ","
-    print constr.rstrip(",")
-
-for i in x4:
-    constr = ""
-    for j in i:
-        constr = constr + str(j) + ","
-    print constr.rstrip(",")
-
-for i in x5:
-    constr = ""
-    for j in i:
-        constr = constr + str(j) + ","
-    print constr.rstrip(",")
-
-for i in x6:
-    constr = ""
-    for j in i:
-        constr = constr + str(j) + ","
-    print constr.rstrip(",")
-
+    print str(src[counter]) + "," + str(dst[counter]) + "," + constr.rstrip(",")
+    counter = counter + 1
+    #print constr.rstrip(",")
     
 #print x1
 #print x2
