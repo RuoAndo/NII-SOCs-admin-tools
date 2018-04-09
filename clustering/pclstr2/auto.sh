ed658d313b169731dab103917c40f27bc73246fb
Ruo Ando <ando.ruo@gmail.com>
2018-04-09 13:54:49 +0900

    auto

--- clustering/pclstr2/auto.sh
+++ clustering/pclstr2/auto.sh
@@ -10,7 +10,8 @@ echo $date
 date_now=`date "+%Y%m%d-%H%M%S"`
 date_start=`date "+%s"`
 
-echo "started at:"$date_now
+echo "started at:"$date_now 
+echo "started at:"$date_now > procTime
 
 #time ./first.sh all-${date} $1 $2 $3
 #time ./second.sh all-${date} $1 $2 $3 $4
@@ -21,6 +22,7 @@ sleep 5s
 
 date_now=`date "+%Y%m%d-%H%M%S"`
 echo "finished at:"$date_now
+echo "finished at:"$date_now >> procTime
 date_end=`date "+%s"`
 
 #echo $date_start
@@ -28,6 +30,10 @@ date_end=`date "+%s"`
 
 diff=`echo $((date_end-date_start))`
 div=`echo $((diff/60))`
-echo "proc time:"$diff"sec"
-echo "proc time:"$div"min"
+
+echo "proc time:"$diff"sec" 
+echo "proc time:"$div"min" 
+
+echo "proc time:"$diff"sec" >> procTime
+echo "proc time:"$div"min" >> procTime
 
