4db527c2ccf292396e085dce426acd53a67500ed
Ruo Ando <ando.ruo@gmail.com>
2018-04-18 10:44:03 +0900

    delta

--- delta/do.sh
+++ delta/do.sh
@@ -1,3 +1,8 @@
+rm -rf delta
+rm -rf delta-sorted
+
+./build.sh delta3
+
 split -l 100000 all -d -a 5
 date_now=`date "+%Y%m%d-%H%M%S"`
 date_start=`date "+%s"`
@@ -19,3 +24,7 @@ echo "proc time:"$div"min"
 
 echo "proc time:"$diff"sec" >> procTime
 echo "proc time:"$div"min" >> procTime
+
+echo "now sorting..."
+
+./sort.pl delta > delta-sorted
