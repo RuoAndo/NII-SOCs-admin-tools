db9abdef9759917b3a66d82fb2710281647c436f
Ruo Ando <ando.ruo@gmail.com>
2018-05-07 10:38:54 +0900

    scripts

---
+++ scripts/cat.sh
@@ -0,0 +1,16 @@
+ls $1* > list
+
+rm -rf all-$1
+touch all-$1
+
+#START_DATE="20140101"
+#END_DATE="20140403"
+#for (( DATE=${START_DATE} ; ${DATE} < ${END_DATE} ; DATE=`date -d "${DATE} 1 day" '+%Y%m%d'` )) ; do
+#  echo ${DATE}
+#done
+
+while read line; do
+    echo $line
+    cat $line >> all-$1
+done < list
+
