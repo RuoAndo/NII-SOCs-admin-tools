c95465b5fc53d28b145f3e14f23c1e57397bc481
Ruo Ando <ando.ruo@gmail.com>
2018-05-11 11:28:20 +0900

    alert

--- scripts/ALERTS/grep.sh
+++ scripts/ALERTS/grep.sh
@@ -1,4 +1,18 @@
 while read line; do
     echo $line
-    grep $line $2 
+
+    while read line2; do
+
+	grep -a $line2 $2 > tmp
+
+	if [ "$?" -eq 0 ]
+	then
+	    gr=`grep -a $line2 $2 | uniq` 
+	    echo 'HIT:'$gr
+	else
+	    echo $line2':NOT FOUND'
+	fi
+
+    done < $line
+
 done < $1
