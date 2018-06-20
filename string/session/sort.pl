4a1c5f24da45efe71f09fff69ab94457c1cb08d5
Ruo Ando <ando.ruo@gmail.com>
2018-06-20 11:25:39 +0900

    c4

--- string/session/sort.pl
+++ string/session/sort.pl
@@ -3,7 +3,7 @@
 my $filein1 = $ARGV[0];
 open(FILEHANDLE1, $filein1);
 @list1 = <FILEHANDLE1>;;
-@list1 = sort { (split(/\,/,$a))[2] <=> (split(/\,/,$b))[2] } @list1;
+@list1 = sort { (split(/\,/,$a))[4] <=> (split(/\,/,$b))[4] } @list1;
 print reverse((@list1));
 
 close(FILEHANDLE1);
