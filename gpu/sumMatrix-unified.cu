5e6ec0b77ecc9bb5eb70280e173be6d520413748
Ruo Ando <ando.ruo@gmail.com>
2018-08-28 15:39:55 +0900

    unified

---
+++ gpu/sumMatrix-unified.cu
@@ -0,0 +1,114 @@
+#include "../common/common.h"
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+void initialData(float *ip, const int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+    {
+        ip[i] = (float)( rand() & 0xFF ) / 10.0f;
+    }
+
+    return;
+}
+
+void sumMatrixOnHost(float *A, float *B, float *C, const int nx, const int ny)
+{
+    float *ia = A;
+    float *ib = B;
+    float *ic = C;
+
+    for (int iy = 0; iy < ny; iy++)
+    {
+        for (int ix = 0; ix < nx; ix++)
+        {
+            ic[ix] = ia[ix] + ib[ix];
+        }
+
+        ia += nx;
+        ib += nx;
+        ic += nx;
+    }
+
+    return;
+}
+
+__global__ void sumMatrixGPU(float *MatA, float *MatB, float *MatC, int nx,
+                             int ny)
+{
+    unsigned int ix = threadIdx.x + blockIdx.x * blockDim.x;
+    unsigned int iy = threadIdx.y + blockIdx.y * blockDim.y;
+    unsigned int idx = iy * nx + ix;
+
+    if (ix < nx && iy < ny)
+    {
+        MatC[idx] = MatA[idx] + MatB[idx];
+    }
+}
+
+int main(int argc, char **argv)
+{
+    printf("%s Start with ", argv[0]);
+
+    int dev = 0;
+    cudaDeviceProp deviceProp;
+    CHECK(cudaGetDeviceProperties(&deviceProp, dev));
+    printf("using Device %d: %s\n", dev, deviceProp.name);
+    CHECK(cudaSetDevice(dev));
+
+    int nx, ny;
+    int ishift = 12;
+
+    if  (argc > 1) ishift = atoi(argv[1]);
+
+    nx = ny = 1 << ishift;
+
+    int nxy = nx * ny;
+    int nBytes = nxy * sizeof(float);
+    printf("Matrix size: nx %d ny %d\n", nx, ny);
+
+    float *A, *B, *hostRef, *gpuRef;
+    CHECK(cudaMallocManaged((void **)&A, nBytes));
+    CHECK(cudaMallocManaged((void **)&B, nBytes));
+    CHECK(cudaMallocManaged((void **)&gpuRef,  nBytes);  );
+    CHECK(cudaMallocManaged((void **)&hostRef, nBytes););
+
+    double iStart = seconds();
+    initialData(A, nxy);
+    initialData(B, nxy);
+    double iElaps = seconds() - iStart;
+    printf("initialization: \t %f sec\n", iElaps);
+
+    memset(hostRef, 0, nBytes);
+    memset(gpuRef, 0, nBytes);
+
+    iStart = seconds();
+    sumMatrixOnHost(A, B, hostRef, nx, ny);
+    iElaps = seconds() - iStart;
+    printf("sumMatrix on host:\t %f sec\n", iElaps);
+
+    int dimx = 32;
+    int dimy = 32;
+    dim3 block(dimx, dimy);
+    dim3 grid((nx + block.x - 1) / block.x, (ny + block.y - 1) / block.y);
+
+    iStart = seconds();
+
+    sumMatrixGPU<<<grid, block>>>(A, B, gpuRef, nx, ny);
+
+    CHECK(cudaDeviceSynchronize());
+    iElaps = seconds() - iStart;
+    printf("sumMatrix on gpu :\t %f sec <<<(%d,%d), (%d,%d)>>> \n", iElaps,
+            grid.x, grid.y, block.x, block.y);
+
+    CHECK(cudaFree(A));
+    CHECK(cudaFree(B));
+    CHECK(cudaFree(hostRef));
+    CHECK(cudaFree(gpuRef));
+
+    CHECK(cudaDeviceReset());
+
+    return (0);
+}
