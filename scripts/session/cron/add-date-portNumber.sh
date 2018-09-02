4f93ca66ba12731214ec27eac8aeeef1bba365e2
Ruo Ando <ando.ruo@gmail.com>
2018-09-02 16:52:15 +0900

    auto

--- scripts/session/cron/add-date-portNumber.sh
+++ scripts/session/cron/add-date-portNumber.sh
@@ -1,14 +1,14 @@
-if [ "$3" = "" ]
+if [ "$1" = "" ]
 then
-    echo "./add-date-portNumber.sh start_date end_date DIR"
+    echo "./add-date-portNumber.sh DIR"
     exit 1
 fi
 
-#START_DATE=`date --date '4 day ago' +%Y%m%d`
-#END_DATE=`date --date '4 day ago' +%Y%m%d`
+START_DATE=`date --date '4 day ago' +%Y%m%d`
+END_DATE=`date --date '4 day ago' +%Y%m%d`
 
-START_DATE=$1
-END_DATE=$2
+#START_DATE=$1
+#END_DATE=$2
 
 rm -rf all-dest_port
 touch all-dest_port
@@ -79,8 +79,8 @@ done
 
 ./build.sh count_destPort_final 
 ./count_destPort_final all-dest_port
-scp destPort_final scp 192.168.72.5:/mnt/sdc/splunk-session/$3
+scp destPort_final scp 192.168.72.5:/mnt/sdc/splunk-session/$1
 
 ./build.sh count_sourcePort_final
 ./count_sourcePort_final all-source_port
-scp sourcePort_final scp 192.168.72.5:/mnt/sdc/splunk-session/$3
+scp sourcePort_final scp 192.168.72.5:/mnt/sdc/splunk-session/$1
