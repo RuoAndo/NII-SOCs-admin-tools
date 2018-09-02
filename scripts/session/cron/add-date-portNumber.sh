67eb21cf675378bbcc071769d2d25aa6863ed2dc
Ruo Ando <ando.ruo@gmail.com>
2018-09-02 21:51:40 +0900

    atprt

--- scripts/session/cron/add-date-portNumber.sh
+++ scripts/session/cron/add-date-portNumber.sh
@@ -77,10 +77,14 @@ for (( DATE=${START_DATE} ; ${DATE} <= ${END_DATE} ; DATE=`date -d "${DATE} 1 da
   cd ..
 done
 
+nLines=`wc -l all-dest_port | cut -d " " -f 1`
 ./build.sh count_destPort_final 
-./count_destPort_final all-dest_port
-scp destPort_final scp 192.168.72.5:/mnt/sdc/splunk-session/$1
+./count_destPort_final all-dest_port $nLines
+cp destPort_final destPort_final-${START_DATE}-${END_DATE}
+scp destPort_final-${START_DATE}-${END_DATE} 192.168.72.5:/mnt/sdc/splunk-session/$1
 
+nLines=`wc -l all-source_port | cut -d " " -f 1`
 ./build.sh count_sourcePort_final
-./count_sourcePort_final all-source_port
-scp sourcePort_final scp 192.168.72.5:/mnt/sdc/splunk-session/$1
+./count_sourcePort_final all-source_port $nLines
+cp sourcePort_final sourcePort_final-${START_DATE}-${END_DATE}
+scp sourcePort_final-${START_DATE}-${END_DATE} 192.168.72.5:/mnt/sdc/splunk-session/$1
