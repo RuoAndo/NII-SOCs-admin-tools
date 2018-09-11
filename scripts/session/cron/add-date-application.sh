0b572367da4391f85dc11560e3c6212151033d3f
Ruo Ando <ando.ruo@gmail.com>
2018-09-11 21:36:58 +0900

    cntapp

---
+++ scripts/session/cron/add-date-application.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+if [ "$1" = "" ]
+then
+    echo "./add-date-application.sh DIR"
+    exit 1
+fi
+
+START_DATE=`date --date '4 day ago' +%Y%m%d`
+END_DATE=`date --date '4 day ago' +%Y%m%d`
+
+#START_DATE=$1
+#END_DATE=$2
+
+rm -rf all-application
+touch all-application
+
+echo "timestamp, application, counted" >> all-application
+
+for (( DATE=${START_DATE} ; ${DATE} <= ${END_DATE} ; DATE=`date -d "${DATE} 1 day" '+%Y%m%d'` )) ; do
+
+  echo ${DATE}
+
+  cp add-date-application.py ${DATE}
+
+  cp *.sh ${DATE}
+  cp *.cpp ${DATE}
+  cp *.h ${DATE}
+  cp *.hpp ${DATE}
+
+  cd ${DATE}
+
+  #####
+
+  #echo "catting...."
+  #./cat.sh
+  
+  #echo "spliting..."
+  #split -l 100000000 all spl.
+
+  ls spl.* > list-spl
+
+  rm -rf tmp-application
+  touch tmp-application
+  
+  while read line; do
+    echo "counting lines..."
+    echo $line
+    nLines=`wc -l ${line} | cut -d " " -f 1`
+    echo $nLines
+
+    echo "count application"
+    time ./build.sh count_application
+    ./count_application ${line} $nLines
+    cat application >> tmp-application
+
+  done < list-spl
+
+  # \cp tmp-dest dest_port
+  # \cp tmp-source source_port
+
+  #####
+
+  python add-date-application.py tmp-application ${DATE} >> ../all-application
+
+  cd ..
+done
+
+nLines=`wc -l all-pair-ip-port | cut -d " " -f 1`
+./build.sh count_application
+./count_application_final all-application $nLines
+cp application_final application_final-${START_DATE}-${END_DATE}
+scp application_final-${START_DATE}-${END_DATE} 192.168.72.5:/mnt/sdc/splunk-session/$1
+
