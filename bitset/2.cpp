bfbe73c69b833e674a410a10d2d95d06f65e7d81
Ruo Ando <ando.ruo@gmail.com>
2018-09-22 21:25:42 +0900

    rm

--- bitset/2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <bitset>
-int main()
-{
-  unsigned long value = 1234;
-  std::cout << value << std::endl; 
-  std::cout << std::bitset<32>(value) << std::endl;
-  return 0;
-}
--- bitset/build.sh
+++
@@ -1,1 +0,0 @@
-g++ -o $1 $1.cpp csv.cpp -fpermissive -std=c++11 -ltbb   
