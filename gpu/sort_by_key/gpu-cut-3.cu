33c7e51835299b8ec375e9ac2b3fe12c98e512e7
Ruo Ando <ando.ruo@gmail.com>
2018-10-12 17:49:37 +0900

    gpu-sort-by-key

---
+++ gpu/sort_by_key/gpu-cut-3.cu
@@ -0,0 +1,192 @@
+#if __linux__ && defined(__INTEL_COMPILER)
+#define __sync_fetch_and_add(ptr,addend) _InterlockedExchangeAdd(const_cast<void*>(reinterpret_cast<volatile void*>(ptr)), addend)
+#endif
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+#include "tbb/concurrent_hash_map.h"
+#include "tbb/blocked_range.h"
+#include "tbb/parallel_for.h"
+#include "tbb/tick_count.h"
+#include "tbb/task_scheduler_init.h"
+//  #include "tbb/tbb_allocator.hz"
+#include "utility.h"
+
+#include "csv.hpp"
+typedef std::basic_string<char,std::char_traits<char>,tbb::tbb_allocator<char> > MyString;
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/sort.h>
+#include <thrust/copy.h>
+#include <algorithm>
+#include <cstdlib>
+#include "util.h"
+
+using namespace tbb;
+using namespace std;
+
+static bool verbose = false;
+static bool silent = false;
+
+const int size_factor = 2;
+
+// typedef concurrent_hash_map<MyString,int> StringTable;
+typedef concurrent_hash_map<MyString,std::vector<string>> StringTable;
+std::vector<string> v_timestamp;
+
+struct Tally {
+    StringTable& table;
+    Tally( StringTable& table_ ) : table(table_) {}
+    void operator()( const blocked_range<MyString*> range ) const {
+      int counter = 0;
+      for( MyString* p=range.begin(); p!=range.end(); ++p ) {
+	StringTable::accessor a;
+	table.insert( a, *p );
+	// a->second += 1;
+	a->second.push_back(v_timestamp[counter]);
+	counter = counter + 1;
+        }
+    }
+};
+
+static MyString* Data;
+
+static void CountOccurrences(int nthreads, int N) {
+    StringTable table;
+
+    tick_count t0 = tick_count::now();
+    parallel_for( blocked_range<MyString*>( Data, Data+N, 10000 ), Tally(table) );
+    tick_count t1 = tick_count::now();
+    
+    int n = 0;
+    int counter = 0;
+
+    thrust::host_vector<const char *> h_vec_1(N);
+    thrust::host_vector<int> h_vec_2(N);
+
+    /*
+    thrust::host_vector<int> h_vec_3(1024 << 8);    
+    std::generate(h_vec_3.begin(), h_vec_3.end(), rand);  
+    */
+
+    for( StringTable::iterator i=table.begin(); i!=table.end(); ++i ) {
+
+      for(size_t c = i->second[0].find_first_of("\""); c != string::npos; c = c = i->second[0].find_first_of("\"")){
+	i->second[0].erase(c,1);
+      }
+
+      std::vector<string>::iterator itr = i->second.begin();
+      for(auto itr = i->second.begin(); itr != i->second.end(); ++itr) {
+	h_vec_1[counter] = (i->first.c_str());
+	h_vec_2[counter] = atoi(i->second[0].c_str());
+      }
+      
+      counter = counter + 1;
+    }
+
+    printf("total = %d  unique = %u  time = %g\n", n, unsigned(table.size()), (t1-t0).seconds());
+
+    thrust::device_vector<const char *> d_vec_1 = h_vec_1;
+    thrust::device_vector<int> d_vec_2 = h_vec_2;
+    thrust::device_vector<int> d_vec_3 = h_vec_3;
+
+    for(int i=0; i<10; i++)
+      std::cout << h_vec_1[i] << "," << h_vec_2[i] << endl;
+
+    thrust::sort_by_key(d_vec_2.begin(), d_vec_2.end(), d_vec_1.begin(), thrust::greater<int>());
+
+    thrust::copy(d_vec_1.begin(), d_vec_1.end(), h_vec_1.begin());
+    thrust::copy(d_vec_2.begin(), d_vec_2.end(), h_vec_2.begin());
+    thrust::copy(d_vec_3.begin(), d_vec_3.end(), h_vec_3.begin());
+
+    std::cout << "sorted:" << endl;
+
+    ofstream outputfile("tmp");  
+
+    for(int i=0; i< h_vec_2.size(); i++)
+      outputfile << h_vec_2[i] << "," << h_vec_1[i] << endl;
+
+    outputfile.close();
+
+}
+
+int main( int argc, char* argv[] ) {
+
+  int counter = 0;
+  int N = atoi(argv[2]);  
+
+    try {
+        tbb::tick_count mainStartTime = tbb::tick_count::now();
+        srand(2);
+
+        utility::thread_number_range threads(tbb::task_scheduler_init::default_num_threads,0);
+
+        if ( silent ) verbose = false;
+
+        Data = new MyString[N];
+
+	const string csv_file = std::string(argv[1]); 
+	vector<vector<string>> data; 
+
+	try {
+	  Csv objCsv(csv_file);
+	  if (!objCsv.getCsv(data)) {
+	    cout << "read ERROR" << endl;
+	    return 1;
+	  }
+
+	  for (unsigned int row = 0; row < data.size(); row++) {
+	    vector<string> rec = data[row]; 
+	    std::string pair = rec[1] + "," + rec[2];
+	    
+	    char* cstr = new char[pair.size() + 1]; 
+	    std::strcpy(cstr, pair.c_str());        
+
+	    Data[row] += cstr;
+	    
+	    v_timestamp.push_back(rec[4].c_str());
+	    
+	    delete[] cstr; 
+	  }
+	}
+	catch (...) {
+	  cout << "EXCEPTION!" << endl;
+	  return 1;
+	}
+	
+        if ( threads.first ) {
+            for(int p = threads.first;  p <= threads.last; p = threads.step(p)) {
+                if ( !silent ) printf("threads = %d  ", p );
+                task_scheduler_init init( p );
+                CountOccurrences( p, N );
+            }
+        } else { // Number of threads wasn't set explicitly. Run serial and parallel version
+            { // serial run
+	      // if ( !silent ) printf("serial run   ");
+	      //  task_scheduler_init init_serial(1);
+              //  CountOccurrences(1);
+            }
+            { // parallel run (number of threads is selected automatically)
+                if ( !silent ) printf("parallel run ");
+                task_scheduler_init init_parallel;
+                CountOccurrences(0, N);
+            }
+        }
+
+        delete[] Data;
+
+        utility::report_elapsed_time((tbb::tick_count::now() - mainStartTime).seconds());
+       
+        return 0;
+	
+    } catch(std::exception& e) {
+        std::cerr<<"error occurred. error text is :\"" <<e.what()<<"\"\n";
+    }
+}
