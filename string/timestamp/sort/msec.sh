8a3c4048f9bfe07b1a49dd49bc75f11cfd82c73e
Ruo Ando <ando.ruo@gmail.com>
2018-11-02 14:10:25 +0900

    msec

---
+++ string/timestamp/sort/msec.sh
@@ -0,0 +1,23 @@
+# about 4 min
+time split -l 100000000 all-org
+./build-gpu.sh 8
+./build-gpu.sh 9
+
+rm -rf tmp-all
+touch tmp-all
+
+ls x* > list
+while read line; do
+    echo $line
+    ./8 $line 100000000
+    cat tmp >> tmp-all
+done < list
+
+nLines=`wc -l tmp-all`
+
+# DATETIME=`date +%Y%m%d_%H%M%S`
+DATETIME=`date +%Y%m%d`
+time ./9 tmp-all $nLines
+cp tmp msec-$DATETIME
+
+scp msec-$DATETIME 192.168.72.5:/mnt/sdc/splunk-msec/
