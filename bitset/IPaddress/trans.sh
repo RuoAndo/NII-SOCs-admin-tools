53cc2b5d1c123dba375292ea1b93744e8a1bcda6
Ruo Ando <ando.ruo@gmail.com>
2018-11-06 16:46:11 +0900

    trans

--- bitset/IPaddress/trans.sh
+++ bitset/IPaddress/trans.sh
@@ -4,15 +4,38 @@ echo $DATE
 cp ${DATE}/all-org .
 
 ./build.sh trans-vector6
+./build.sh trans-vector7
+
+rm -rf x*
 
 time split -l 100000000 all-org
 
-rm -rf trans-all
-touch trans-all
+rm -rf trans-pair-all
+touch trans-pair-all
 
 ls x* > list
 while read line; do
     echo $line
     ./trans-vector6 $line 100000000
-    cat trans >> trans-all
+    cat trans >> trans-pair-all
 done < list
+
+rm -rf trans-src-dst-all
+touch trans-src-dst-all
+
+ls x* > list
+while read line; do
+    echo $line
+    ./trans-vector7 $line 100000000
+    cat trans >> trans-src-dst-all
+done < list
+
+cp trans-pair-all trans-pair-all-${DATE}
+cp trans-src-dst-all trans-src-dst-all-${DATE}
+
+scp trans-pair-all-${DATE} 192.168.76.210:/mnt/data/matrix
+scp trans-src-dst-all-${DATE} 192.168.76.210:/mnt/data/matrix
+
+scp trans-pair-all-${DATE} 192.168.76.212:/mnt/data2/matrix
+scp trans-src-dst-all-${DATE} 192.168.76.212:/mnt/data2/matrix
+
