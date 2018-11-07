1bf5704925e9682401e98acf7c04b63958d77d32
Ruo Ando <ando.ruo@gmail.com>
2018-11-07 17:33:22 +0900

    matrix

---
+++ session/matrix/sort.cu
@@ -0,0 +1,109 @@
+#if __linux__ && defined(__INTEL_COMPILER)
+#define __sync_fetch_and_add(ptr,addend) _InterlockedExchangeAdd(const_cast<void*>(reinterpret_cast<volatile void*>(ptr)), addend)
+#endif
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <bitset>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "tbb/concurrent_hash_map.h"
+#include "tbb/blocked_range.h"
+#include "tbb/parallel_for.h"
+#include "tbb/tick_count.h"
+#include "tbb/task_scheduler_init.h"
+//  #include "tbb/tbb_allocator.hz"
+#include "utility.h"
+
+#include "csv.hpp"
+typedef std::basic_string<char,std::char_traits<char>,tbb::tbb_allocator<char> > MyString;
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/sort.h>
+#include <thrust/copy.h>
+#include <algorithm>
+#include <cstdlib>
+#include "util.h"
+
+using namespace tbb;
+using namespace std;
+
+int main( int argc, char* argv[] ) {
+
+  // int counter = 0;
+    int N = atoi(argv[2]);
+    
+    thrust::host_vector<long> h_vec_1(N);
+    thrust::host_vector<long> h_vec_2(N);
+
+    static MyString* Data;
+
+    try {
+        tbb::tick_count mainStartTime = tbb::tick_count::now();
+        srand(2);
+
+        utility::thread_number_range threads(tbb::task_scheduler_init::default_num_threads,0);
+        // if ( silent ) verbose = false;
+
+
+        Data = new MyString[N];
+
+	const string csv_file = std::string(argv[1]); 
+	vector<vector<string>> data; 
+
+	try {
+	  Csv objCsv(csv_file);
+	  if (!objCsv.getCsv(data)) {
+	    cout << "read ERROR" << endl;
+	    return 1;
+	  }
+	 
+      	  for (int row = 0; row < data.size(); row++) {
+	    vector<string> rec = data[row]; 
+	    std::string timestamp = rec[0];
+	    std::string addr = rec[1];
+
+	    h_vec_1.push_back(std::atol(timestamp.c_str()));
+	    h_vec_2.push_back(std::atol(addr.c_str()));
+
+	    }
+
+	  thrust::device_vector<long> key_in = h_vec_2;
+	  thrust::device_vector<long> value_in = h_vec_1; 
+
+	  // thrust::sort_by_(key_in.begin(), key_in.end());
+	  // long new_size = new_end.first - dkey_out.begin();
+
+	  thrust::sort_by_key(key_in.begin(), key_in.end(), value_in.begin(),thrust::greater<int>());
+
+	  for(long i=0; i <10; i++)
+	    {
+		std::cout << key_in[i] << "," << value_in[i] << endl;
+	    }
+
+	  // outputfile.close();
+	}
+	
+	catch (...) {
+	  cout << "EXCEPTION!" << endl;
+	  return 1;
+	}
+	
+        delete[] Data;
+        utility::report_elapsed_time((tbb::tick_count::now() - mainStartTime).seconds());
+       
+        return 0;
+	
+    } catch(std::exception& e) {
+        std::cerr<<"error occurred. error text is :\"" <<e.what()<<"\"\n";
+    }
+}
