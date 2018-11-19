d1ed194e9281626b1bc4a139520827bc35891e02
Ruo Ando <ando.ruo@gmail.com>
2018-11-19 15:12:21 +0900

    div

---
+++ session/div/group2.cpp
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <pthread.h>
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <eigen3/Eigen/Dense>
+
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/SVD>
+
+#include <random>
+#include <map>
+
+#define THREAD_NUM 1
+
+using namespace Eigen;
+using namespace std;
+
+typedef struct _thread_arg {
+    int id;
+} thread_arg_t;
+
+typedef struct _result {
+  map<string, string> m;
+  pthread_mutex_t mutex;
+} result_t;
+result_t result;
+
+void thread_func(void *arg) {
+    thread_arg_t* targ = (thread_arg_t *)arg;
+    int i, j, k;
+    int label = 0;
+    unsigned int key = 0;
+    unsigned int value = 0;
+    int counter = 0;
+    string src;
+    string dst;
+    
+    string fname = std::to_string(targ->id);
+    std::cout << "thread ID: " << targ->id << " - start." << std::endl;
+
+    ifstream ifs(fname);
+ 
+    string str;
+    while(getline(ifs,str)){
+      string token;
+      istringstream stream(str);
+      counter = 0;
+      while(getline(stream,token,',')){
+	
+	if(counter==0)
+	  src = token;
+
+	if(counter==4)
+	  dst = token;
+
+	std::cout << src << "," << dst << endl;
+
+	pthread_mutex_lock(&result.mutex);
+	result.m.insert(pair<string, string>(src,dst));
+	pthread_mutex_unlock(&result.mutex);
+	
+      counter = counter + 1;
+      }
+
+    }
+
+    std::cout << "thread ID: " << targ->id << " - done." << std::endl;
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t handle[THREAD_NUM];
+    thread_arg_t targ[THREAD_NUM];
+
+    int i;
+
+    /* ˆ—ŠJŽn */
+    for (i = 0; i < THREAD_NUM; i++) {
+        targ[i].id = i;
+        pthread_create(&handle[i], NULL, (void*)thread_func, (void*)&targ[i]);
+    }
+
+    /* I—¹‚ð‘Ò‚Â */
+    for (i = 0; i < THREAD_NUM; i++) 
+        pthread_join(handle[i], NULL);
+
+    map<string, string>::iterator itr;   
+    
+    // counter = 0;
+    for (itr = result.m.begin(); itr != result.m.end(); itr++)
+      {
+	std::cout << itr->first << "," << itr->second << std::endl;
+
+	/*
+	if(counter==10)
+	  break;
+	*/	
+
+	// counter = counter + 1;
+      }
+    
+    std::cout << "map size() is " << result.m.size() << std::endl;
+}
