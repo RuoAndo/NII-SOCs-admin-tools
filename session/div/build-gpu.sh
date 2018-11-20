8d1ea2cb7f348d7bf213af72e29ba538fbb1448f
Ruo Ando <ando.ruo@gmail.com>
2018-11-20 12:52:40 +0900

    group3

---
+++ session/div/build-gpu.sh
@@ -0,0 +1,7 @@
+rm -rf a.out
+#g++ -c csv.cpp -std=c++11
+#/usr/local/cuda-9.2/bin/nvcc -g -o $1 csv.o $1.cu -std=c++11 -ltbb
+
+#g++ -c csv.cpp -std=c++11
+/usr/local/cuda-9.2/bin/nvcc -g -o $1 $1.cu -std=c++11 -lpthread #--fpermissive
+#/usr/local/cuda-9.2/bin/nvcc -g -o $1 $1.cu -lpthread -lpthread
