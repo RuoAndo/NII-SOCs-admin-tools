61af581ef83f7062f6c6620bfc644299649593bd
Ruo Ando <ando.ruo@gmail.com>
2018-12-02 17:12:06 +0900

    auto

--- session/div/do.sh
+++ session/div/do.sh
@@ -1,9 +1,11 @@
-#head -n 1500000000 all-org > all-org-cut
+head -n 2100000000 all-whole > all-org-cut
 nLines=300000000
 
 ./build-gpu.sh group10
-#echo "now spliting..."
-#split -l 300000000 all-org-cut
+echo "now spliting..."
+
+rm -rf x*
+split -l $nLines all-org-cut
 
 ls xa* > list
 
@@ -13,7 +15,7 @@ touch result-all
 while read line; do
     echo $line
     split -d -l 10000000 $line
-    ./group10 133.92.253.33 $nLines
+    ./group10 116.214.87.171 $nLines
     \cp result result.${line}
     cat result >> result-all
 done < list
