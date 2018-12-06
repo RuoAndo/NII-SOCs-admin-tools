89dce87ae5372fe640f9af269aee610740215154
Ruo Ando <ando.ruo@gmail.com>
2018-12-06 21:45:42 +0900

    auto

--- DSL/1/src/main/scala/example/1.scala
+++ DSL/1/src/main/scala/example/1.scala
@@ -36,14 +36,8 @@ def address_format(line : String) = {
     list.foreach(list => println(list))
     // println(list)
 
-    //郵便番号 zip = dropQuote(list(2))
-
-    //住所
-    //    val address = dropQuote(list(6)) + dropQuote(list(7)) + dropQuote(list(8))
-    //	    out.write("%s,%s\n" format (zip, address))
 }
 
-// 前後のダブルクォーテーションを除去
 def dropQuote(str : String) : String = {
 	        str drop 1 dropRight 1
 }
@@ -54,16 +48,16 @@ def dropQuote(str : String) : String = {
       new contentBuilder(content)
       
   {
-    println("-- ファイルの内容を読み込み --")
+
 
     val filename = args(0)
-    // ファイルからSourceのインスタンスを生成
+
     val source = Source.fromFile(filename, "UTF-8")
 
     var counter = 0;
 
     try {
-      // 1行毎の文字列を返すIteratorを取得して表示
+
           counter = 0;
 	  source.getLines().foreach { line: String =>
           // println(line)
@@ -73,7 +67,7 @@ def dropQuote(str : String) : String = {
 	  // counter = counter + 1;
       }
     } finally {
-      // クローズ
+
       source.close()
     }
   }
