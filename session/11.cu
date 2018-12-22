5b9416f93fa55c2aa369bf13fa1574b1736b1101
Ruo Ando <ando.ruo@gmail.com>
2018-12-22 18:40:26 +0900

    11

---
+++ session/11.cu
@@ -0,0 +1,236 @@
+#if __linux__ && defined(__INTEL_COMPILER)
+#define __sync_fetch_and_add(ptr,addend) _InterlockedExchangeAdd(const_cast<void*>(reinterpret_cast<volatile void*>(ptr)), addend)
+#endif
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <bitset>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "tbb/concurrent_hash_map.h"
+#include "tbb/blocked_range.h"
+#include "tbb/parallel_for.h"
+#include "tbb/tick_count.h"
+#include "tbb/task_scheduler_init.h"
+//  #include "tbb/tbb_allocator.hz"
+#include "utility.h"
+
+#include "csv.hpp"
+typedef std::basic_string<char,std::char_traits<char>,tbb::tbb_allocator<char> > MyString;
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/sort.h>
+#include <thrust/copy.h>
+#include <algorithm>
+#include <cstdlib>
+#include "util.h"
+
+#include "timer.h"
+
+using namespace tbb;
+using namespace std;
+
+typedef concurrent_hash_map<MyString,std::vector<string>> StringTable;
+std::vector<string> v_pair;
+std::vector<string> v_count;
+static MyString* Data;
+
+std::vector<std::string> split_string_2(std::string str, char del) {
+  int first = 0;
+  int last = str.find_first_of(del);
+
+  std::vector<std::string> result;
+
+  while (first < str.size()) {
+    std::string subStr(str, first, last - first);
+
+    result.push_back(subStr);
+
+    first = last + 1;
+    last = str.find_first_of(del, first);
+
+    if (last == std::string::npos) {
+      last = str.size();
+    }
+  }
+
+  return result;
+}
+
+int main( int argc, char* argv[] ) {
+
+  int N = atoi(argv[2]);  
+  unsigned int t, elapsedTime; 
+  
+  thrust::host_vector<long> h_vec_1(N);
+  thrust::host_vector<long> h_vec_2(N);   
+
+    try {
+        tbb::tick_count mainStartTime = tbb::tick_count::now();
+        srand(2);
+
+	std::cout << "allocating long string ..." << N << endl;
+	start_timer(&t);
+	
+        Data = new MyString[N];
+	
+	std::cout << "done." << endl;
+	elapsedTime = stop_timer(&t);
+	print_timer(elapsedTime); 
+
+	const string csv_file = std::string(argv[1]);
+	vector<vector<string>> data; 
+
+	try {
+	
+          std::cout << "reading big file ..." << endl;
+	  start_timer(&t);
+
+	  Csv objCsv(csv_file);
+	  if (!objCsv.getCsv(data)) {
+	    cout << "read ERROR" << endl;
+	    return 1;
+	  }
+
+	  std::cout << "done." << endl;
+	  elapsedTime = stop_timer(&t);
+	  print_timer(elapsedTime); 
+	 
+      	  for (int row = 0; row < data.size(); row++) {
+
+	    // std::cout << "inside the loop ..." << endl;
+
+	    vector<string> rec = data[row]; 
+	    std::string timestamp = rec[0];
+
+	    for(size_t c = timestamp.find_first_of("\""); c != string::npos; c = c = timestamp.find_first_of("\"")){
+	      timestamp.erase(c,1);
+	    }
+	    for(size_t c = timestamp.find_first_of("."); c != string::npos; c = c = timestamp.find_first_of(".")){
+	      timestamp.erase(c,1);
+	    }
+	    for(size_t c = timestamp.find_first_of(" "); c != string::npos; c = c = timestamp.find_first_of(" ")){
+	      timestamp.erase(c,1);
+	    }
+	    for(size_t c = timestamp.find_first_of(":"); c != string::npos; c = c = timestamp.find_first_of(":")){
+	      timestamp.erase(c,1);
+	    }
+	    for(size_t c = timestamp.find_first_of("/"); c != string::npos; c = c = timestamp.find_first_of("/")){
+	      timestamp.erase(c,1);
+	    }
+
+	    std::string bytes = rec[21];
+	    for(size_t c = bytes.find_first_of("\""); c != string::npos; c = c = bytes.find_first_of("\"")){
+	      bytes.erase(c,1);
+	    }
+
+	    // 2018 09 20 00 00 00 133	    
+	    // timestamp.pop_back();
+	    // timestamp.pop_back();
+	    // timestamp.pop_back();
+
+	    h_vec_1.push_back(std::atol(timestamp.c_str()));
+	    h_vec_2.push_back(std::atol(bytes.c_str()));
+
+	    if(row%10000000==0)
+		std::cout << "storing host__vector..." << row << endl;
+
+	    }
+
+	    std::cout << "done." << endl;
+	    elapsedTime = stop_timer(&t);
+	    print_timer(elapsedTime); 
+
+	    std::cout << "storing device vector ..." << endl;
+	    start_timer(&t);
+
+	    thrust::device_vector<long> key_in = h_vec_1;
+	    thrust::device_vector<long> value_in = h_vec_2; 
+
+	    std::cout << "done." << endl;
+	    elapsedTime = stop_timer(&t);
+	    print_timer(elapsedTime); 
+
+	  // thrust::sort(key_in.begin(), key_in.end(),thrust::greater<int>());
+
+          std::cout << "sort and reduction by key ..." << endl;
+	  start_timer(&t);
+
+	  thrust::sort(key_in.begin(), key_in.end());
+
+	  thrust::device_vector<long> dkey_out(N,0);
+	  thrust::device_vector<long> dvalue_out(N,0);
+	  
+	  auto new_end = thrust::reduce_by_key(key_in.begin(),
+					       key_in.end(),
+					       value_in.begin(),
+					       dkey_out.begin(),
+	  				       dvalue_out.begin());
+
+	  long new_size = new_end.first - dkey_out.begin();
+
+	  std::cout << "done." << endl;
+	  elapsedTime = stop_timer(&t);
+	  print_timer(elapsedTime); 
+
+   	  std::remove("tmp");
+	  ofstream outputfile("tmp");
+
+          std::cout << "writing file ..." << endl;
+	  start_timer(&t);
+
+	  for(long i=0; i <new_size; i++)
+	    {
+		if(std::to_string(dkey_out[i]).length()  == 17)
+		{
+			      outputfile << dkey_out[i] << "," << dvalue_out[i] << endl;
+			      // 2018 09 20 00 00 00 133
+			      /*
+			      std::string tmpstring = std::to_string(dkey_out[i]);
+			      outputfile << tmpstring.substr( 0, 4 )
+			      << "-"
+			      << tmpstring.substr( 4, 2 ) 
+			      << "-"
+			      << tmpstring.substr( 6, 2 ) 
+			      << " "
+			      << tmpstring.substr( 8, 2 ) 
+			      << ":"
+			      << tmpstring.substr( 10, 2 )
+			      << ":"
+			      << tmpstring.substr( 12, 2 )
+			      << "," << dvalue_out[i] << endl;
+			      */
+	  	}
+	    }
+	  // std::cout << std::endl;
+
+	  std::cout << "done." << endl;
+	  elapsedTime = stop_timer(&t);
+	  print_timer(elapsedTime); 
+
+	  outputfile.close();
+	}
+
+	catch (...) {
+	  cout << "EXCEPTION!" << endl;
+	  return 1;
+	}
+	
+        delete[] Data;
+        utility::report_elapsed_time((tbb::tick_count::now() - mainStartTime).seconds());
+       
+        return 0;
+	
+    } catch(std::exception& e) {
+        std::cerr<<"error occurred. error text is :\"" <<e.what()<<"\"\n";
+    }
+}
