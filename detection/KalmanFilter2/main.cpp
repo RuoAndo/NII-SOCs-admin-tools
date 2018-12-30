598fd89ee673ab65136f11699a2a067459147f5e
Ruo Ando <ando.ruo@gmail.com>
2018-12-30 21:50:57 +0900

    kf

--- detection/KalmanFilter2/main.cpp
+++ detection/KalmanFilter2/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include "KalmanFilter.h"
+#include "timer.h"
 
 // #define LINE_NUM 5000
 
@@ -27,14 +28,18 @@ int main(int argc, char const *argv[])
   int i;
   int nData;
 
+  unsigned int t, travdirtime; 
+  
   nData = atoi(argv[2]);
   float* measure = new float[atoi(argv[2])];
+  float* org = new float[atoi(argv[2])];
+  float* diff = new float[atoi(argv[2])-1];
 
   readFromFile(argv[1], int(argv[2]), measure);
 
   for(i=0;i<nData;i++)
     {
-      cout << measure[i] << endl;
+      // cout << measure[i] << endl;
     }
   
   MatrixXf A(1, 1); A << 1;
@@ -51,20 +56,36 @@ int main(int argc, char const *argv[])
 
   VectorXf Z(1);
 
-  ofstream outputfile("tmp");
+  ofstream outputfile("prdct");
+
+  start_timer(&t);
   
   for (int i = 0; i < nData; ++i)
-  {
+  { 
     filter1.predict(); 
     Z << measure[i];
-    filter1.correct( Z ); 
-  
-    cout << measure[i] << "," << filter1.X << endl;
-    outputfile<< filter1.X << endl;
-  
+    filter1.correct( Z );   
+    // cout << measure[i] << "," << filter1.X << endl;
+    outputfile << filter1.X << endl;
+   org[i] = filter1.X[0];
+
+    if (i%(nData/10)==0)
+      filter1.setInitial(X0, P0);
   }
 
+  travdirtime = stop_timer(&t);
+  print_timer(travdirtime);      
+  
   outputfile.close();
 
+  ofstream outputfile2("ma");
+  for (int i = 0; i < nData -1; ++i)
+  {
+    diff[i]=fabs(org[i+1]-org[i]);
+    outputfile2 << diff[i] << endl;
+  }
+
+  outputfile2.close();
+  
   return 0;
 }
---
+++ detection/KalmanFilter2/readplot-compare2.py
@@ -0,0 +1,30 @@
+#coding:utf-8
+# K-means
+
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+from scipy.ndimage.interpolation import shift
+
+argvs = sys.argv
+
+def zscore(x, axis = None):
+    xmean = x.mean(axis=axis, keepdims=True)
+    xstd  = np.std(x, axis=axis, keepdims=True)
+    zscore = abs((x-xmean)/xstd)
+    return zscore
+
+if __name__ == "__main__":
+    data = np.genfromtxt(argvs[1], delimiter=",")
+    data2 = np.genfromtxt(argvs[2], delimiter=",")
+
+    plt.subplot(2, 1, 1)
+    plt.plot(data)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(data2)
+
+    plt.show()  
+        
+
+
