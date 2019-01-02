95be413b9f13f503cdb57e1f0b655b46bf8e9fbf
Ruo Ando <ando.ruo@gmail.com>
2019-01-02 19:09:15 +0900

    auto

--- session/direction/do.sh
+++ session/direction/do.sh
@@ -6,7 +6,8 @@ touch rendered-all
 while read line; do
     echo $line
     rm -rf rendered_$line
-    ./netmask5 monitoring_list2 $line 1000 &
+    nLines_to_process=`wc -l $line | cut -d " " -f 1`
+    ./netmask5 monitoring_list2 $nLines_to_process &
     #cat rendered_$line >> rendered-all
 done < list
 
