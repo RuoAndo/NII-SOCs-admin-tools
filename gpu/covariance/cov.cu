a75434e513032a291a9dda528a795c8c29fd7266
Ruo Ando <ando.ruo@gmail.com>
2019-01-16 10:55:03 +0900

    cov

--- gpu/covariance/cov.cu
+++ gpu/covariance/cov.cu
@@ -1,14 +1,5 @@
 #include <cublas_v2.h>
 
-#include <string>
-#include <cstring>
-#include <cctype>
-#include <cstdlib>
-#include <cstdio>
-#include <iostream>
-#include <fstream>
-#include <bitset>
-
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 #include <thrust/generate.h>
@@ -24,6 +15,7 @@
 #include "TimingGPU.cuh"
 
 #include "csv.hpp"
+using namespace std;
 
 /*************************************/
 /* CONVERT LINEAR INDEX TO ROW INDEX */
@@ -38,27 +30,15 @@ struct linear_index_to_row_index : public thrust::unary_function<T,T> {
     __host__ __device__ T operator()(T i) { return i / Ncols; }
 };
 
-using namespace std;
-
-#define SAMPLE_RATIO 100
-
 /********/
 /* MAIN */
 /********/
 int main( int argc, char* argv[] )
 {
-    int N = atoi(argv[2]);  
-
-    const int Nsamples = 2;
-    // --- Number of realizations for each random variable (number of rows of the X matrix)
-
-    const int NX  = N;
-    // --- Number of random variables (number of columns of the X matrix)
-
-    int counter = 0;
-
-  thrust::host_vector<float> h_vec_1(N);
-  thrust::host_vector<float> h_vec_2(N);   
+  int N = atoi(argv[2]);
+  
+  thrust::host_vector<long> h_vec_1(N);
+  thrust::host_vector<long> h_vec_2(N);   
 
   const string csv_file = std::string(argv[1]); 
   vector<vector<string>> data; 
@@ -69,53 +49,35 @@ int main( int argc, char* argv[] )
      return 1;
   }
 
-  /*
-  std::remove("tmp");
-  ofstream outputfile("tmp");
-  */
-
   for (int row = 0; row < data.size(); row++) {
       vector<string> rec = data[row]; 
       std::string timestamp = rec[0];
 
-      // std::cout << rec[1] << std::endl; 
-
       h_vec_1.push_back(std::stof(rec[0].c_str()));
-      // h_vec_2.push_back(std::stof(rec[1]));
       h_vec_2[row] = std::stof(rec[1]);
-      // h_vec_1[row] = std::stof(rec[0]);
-
-      // std::cout << std::stof(rec[1]) << std::endl;
-      // std::cout << h_vec_2[row] << std::endl; 
   }
 
-  // --- Matrix allocation and initialization
-  thrust::device_vector<float> d_X(Nsamples * NX);
-  thrust::device_vector<float> d_X_bak(Nsamples * NX);
+  thrust::host_vector<long> dout_2(N);
+  thrust::host_vector<long> dout_3(N);
+  thrust::host_vector<long> d_vec_2(N);
+  
+  thrust::copy(h_vec_2.begin(), h_vec_2.end(), d_vec_2.begin());   
+  thrust::inclusive_scan(d_vec_2.begin(), d_vec_2.end(), dout_2.begin());
+  thrust::exclusive_scan(d_vec_2.begin(), d_vec_2.end(), dout_3.begin());
 
-    for (size_t i = 0; i < NX; i++)
-    	{
-	d_X[i] = h_vec_2[i];
-   	d_X_bak[i] = d_X[i];
-	}
+  const int Nsamples = 2;
+    // --- Number of realizations for each random variable (number of rows of the X matrix)
+  const int NX = N;
+    // --- Number of random variables (number of columns of the X matrix)
 
-	// d_X[NX]=0;
-	// d_X_bak[NX]=0;
-	
-    for (size_t i = 0; i < NX-1; i++)
-    	{
-	d_X[i+NX] = d_X[i];
-	d_X_bak[i+NX] = d_X[i];
-	}
+    // --- Matrix allocation and initialization
+  thrust::device_vector<long> d_X(Nsamples * NX);
 
-    // d_X[NX*Nsamples-1] = 0;
-    //d_X_bak[NX*Nsamples-1] = 0;
-    // d_X[NX-1] = 0;
-    // d_X_bak[NX-1] = 0;
-    		  
-
-    // for (size_t i = 0; i < NX * Nsamples; i++)
-    	// std::cout << d_X[i] << std::endl;
+    for(int i=0;i<N;i++)
+    {	    
+    	    d_X[i] = dout_2[i];
+	    d_X[i+NX] = dout_3[i];
+    }
 
     // --- cuBLAS handle creation
     cublasHandle_t handle;
@@ -125,13 +87,13 @@ int main( int argc, char* argv[] )
     /* CALCULATING THE MEANS OF THE RANDOM VARIABLES */
     /*************************************************/
     // --- Array containing the means multiplied by Nsamples
-    thrust::device_vector<float> d_means(NX);
-    thrust::device_vector<float> d_ones(Nsamples, 1.f);
+    thrust::device_vector<long> d_means(NX);
+    thrust::device_vector<long> d_ones(Nsamples, 1.f);
 
-    float alpha = 1.f / (float)Nsamples;
-    float beta  = 0.f;
+    long alpha = 1.f / (long)Nsamples;
+    long beta  = 0.f;
     cublasSgemv(handle, CUBLAS_OP_T, Nsamples, NX, &alpha, thrust::raw_pointer_cast(d_X.data()), Nsamples, 
-                               thrust::raw_pointer_cast(d_ones.data()), 1, &beta, thrust::raw_pointer_cast(d_means.data()), 1);
+                thrust::raw_pointer_cast(d_ones.data()), 1, &beta, thrust::raw_pointer_cast(d_means.data()), 1);
 
     /**********************************************/
     /* SUBTRACTING THE MEANS FROM THE MATRIX ROWS */
@@ -151,8 +113,8 @@ int main( int argc, char* argv[] )
 
     alpha = 1.f;
     cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, NX, NX, Nsamples, &alpha,
-                               thrust::raw_pointer_cast(d_X.data()), Nsamples, thrust::raw_pointer_cast(d_X.data()), Nsamples, &beta,
-                               thrust::raw_pointer_cast(d_cov.data()), NX);
+                thrust::raw_pointer_cast(d_X.data()), Nsamples, thrust::raw_pointer_cast(d_X.data()), Nsamples, &beta,
+                thrust::raw_pointer_cast(d_cov.data()), NX);
 
     // --- Final normalization by Nsamples - 1
     thrust::transform(
@@ -161,24 +123,21 @@ int main( int argc, char* argv[] )
                 d_cov.begin(),
                 thrust::divides<float>());  
 
-    // for(int i = 0; i < N * Nsamples; i++) std::cout << d_X[i] << "\n";
-    // std::cout << "size:"  << d_cov.size() << std::endl;
+    // for(int i = 0; i < NX * NX; i++) std::cout << d_cov[i] << "\n";
 
-    // for (size_t i = 0; i < d_X.size(); i++) std::cout << "d_X_bak:" << d_X_bak[i] << std::endl;
-
-    counter = 0;
+    int counter = 0;
     for(int i = 0; i < NX; i++)
     {
         for(int j = 0; j < NX; j++)
 	{
 		if(i == j)
 		{
-			std::cout << d_X_bak[counter%NX] << "," << d_cov[i+j*NX] << "," << d_X_bak[counter%NX] / d_cov[i+j*NX] << std::endl;
+		std::cout << d_X[counter%NX] << "," << d_cov[i+j*NX] << std::endl;
 		}
 	counter = counter + 1;
 	}
 	
     }
-	
+
     return 0;
 }
