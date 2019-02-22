85f67ae50dd2381b448e27958165e1d6ca36728a
Ruo Ando <ando.ruo@gmail.com>
2019-02-22 16:57:56 +0900

    msec-bytes

---
+++ session/msec-bytes/msec-bytes.sh
@@ -0,0 +1,35 @@
+# about 4 min
+DATE=`date --date '3 day ago' +%Y%m%d`  
+echo $DATE
+cp ${DATE}/all-org .
+
+rm -rf x*
+
+time split -l 100000000 all-org
+./build-gpu.sh 11
+./build-gpu.sh 9
+
+rm -rf tmp-all
+touch tmp-all
+
+ls x* > list
+while read line; do
+    echo $line
+    ./11 $line 100000000
+    cat tmp >> tmp-all
+done < list
+
+nLines=`wc -l tmp-all`
+
+# DATETIME=`date +%Y%m%d_%H%M%S`
+#DATETIME=`date +%Y%m%d`
+time ./9 tmp-all $nLines
+cp tmp msec-bytes-${DATE}
+cp raw raw-bytes-${DATE}
+
+scp msec-bytes-${DATE} 192.168.72.6:/mnt/sdc/splunk-msec-bytes/
+scp msec-bytes-${DATE} 192.168.72.6:/mnt/sdc/splunk-msec-bytes/current
+scp raw-bytes-${DATE} 192.168.72.6:/mnt/sdc/splunk-msec-bytes/raw/
+
+mv msec-bytes-${DATE} ./backup
+mv raw-bytes-${DATE} ./backup
