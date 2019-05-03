6d9d19e1b874cb8974286636a200e55170ab714a
Ruo Ando <ando.ruo@gmail.com>
2019-05-03 13:20:28 +0900

    build

--- session/nvlink/build-traverse.sh
+++ session/nvlink/build-traverse.sh
@@ -1,3 +1,3 @@
 nvcc -g -c $1.cpp csv.cpp -std=c++11 -ltbb -Xcompiler="-fpermissive, -lpthread"
-nvcc -g -c kernel.cu
-nvcc -g -o $1 $1.o csv.o kernel.o -ltbb --generate-code arch=compute_60,code=sm_60
+nvcc -g -c kernel.cu  -std=c++11
+nvcc -g -o $1 $1.o csv.o kernel.o  -std=c++11 -ltbb --generate-code arch=compute_60,code=sm_60
