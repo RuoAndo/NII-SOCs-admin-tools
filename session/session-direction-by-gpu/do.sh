1be350c7c04368721a54cb83924c235060326aee
Ruo Ando <ando.ruo@gmail.com>
2019-05-05 18:29:18 +0900

    auto

--- session/session-direction-by-gpu/do.sh
+++ session/session-direction-by-gpu/do.sh
@@ -16,11 +16,12 @@ time CUDA_VISIBLE_DEVICES=0 ./traverse4 ./${DATE}
 cp tmp-inward tmp-inward-${DATE}
 cp tmp-outward tmp-outward-${DATE}
 
-scp tmp-inward-${DATE} 192.168.72.6:/mnt/sdc/splunk-session-direction-by-gpu/
-scp tmp-outward-${DATE} 192.168.72.6:/mnt/sdc/splunk-session-direction-by-gpu/
 
-scp tmp-inward-${DATE} 192.168.72.6:/mnt/sdc/splunk-session-direction-by-gpu/tmp-inward-current
-scp tmp-outward-${DATE} 192.168.72.6:/mnt/sdc/splunk-session-direction-by-gpu/tmp-outward-current
+scp tmp-inward-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_direction_by_gpu/gpu02/
+scp tmp-outward-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_direction_by_gpu/gpu02/
+
+scp tmp-inward-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_direction_by_gpu/gpu02/tmp-inward-current
+scp tmp-outward-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_direction_by_gpu/gpu02/tmp-outward-current
 
 rm -rf ${DATE}
 rm -rf tmp-inward-${DATE}
