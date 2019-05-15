e1375eff3816488b41e2fae24d47ee7000f1d8d4
Ruo Ando <ando.ruo@gmail.com>
2019-05-15 09:59:28 +0900

    auto

--- session/session-country-by-gpu/do.sh
+++ session/session-country-by-gpu/do.sh
@@ -14,12 +14,14 @@ rm -rf all-org
 cd ..
 time CUDA_VISIBLE_DEVICES=1 ./traverse6 ./${DATE}
 cp tmp-cn-inward tmp-cn-inward-${DATE}
-#cp tmp-ssl tmp-ssl-${DATE}
-#cp tmp-web-browsing tmp-web-browsing-${DATE}
+cp tmp-cn-outward tmp-cn-outward-${DATE}
 
 scp tmp-cn-inward-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_country_by_gpu/gpu04/
-scp tmp-cn-inward-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_application_by_gpu/gpu02/tmp-cn-inward-current
+scp tmp-cn-inward-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_country_by_gpu/gpu04/tmp-cn-inward-current
+scp tmp-cn-outward-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_country_by_gpu/gpu04/
+scp tmp-cn-outward-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_country_by_gpu/gpu04/tmp-cn-outward-current
 
 rm -rf ${DATE}
 rm -rf tmp-cn-inward-${DATE}
+rm -rf tmp-cn-outward-${DATE}
 
