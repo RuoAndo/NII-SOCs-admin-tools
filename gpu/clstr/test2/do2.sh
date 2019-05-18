c89b0ab5140924e60c52ac79cd79ddb048b8e1a9
Ruo Ando <ando.ruo@gmail.com>
2019-05-18 21:41:19 +0900

    auto

--- gpu/clstr/test2/do2.sh
+++ gpu/clstr/test2/do2.sh
@@ -15,7 +15,7 @@ touch atomic.csv
 
 COUNT=10000
 while [ $COUNT -lt $MAX ]; do
-    rslt=`./atomic data2 10000 ${COUNT} 3`
+    rslt=`./atomic data2 ${COUNT} 10000 3`
     echo $rslt","$COUNT | tee -a atomic.csv
     COUNT=`expr $COUNT + $INTVL` # COUNT をインクリメント    
 done
@@ -26,7 +26,7 @@ touch fine-coarse.csv
 echo "fine-coarse"
 COUNT=10000
 while [ $COUNT -lt $MAX ]; do
-    rslt=`./fine-coarse data2 10000 ${COUNT} 3`
+    rslt=`./fine-coarse data2 ${COUNT} 10000 3`
     echo $rslt","$COUNT | tee -a fine-coarse.csv
     COUNT=`expr $COUNT + $INTVL` # COUNT をインクリメント    
 done
@@ -37,11 +37,10 @@ touch thrust.csv
 
 COUNT=10000
 while [ $COUNT -lt $MAX ]; do
-    rslt=`./thrust data2 1000 ${COUNT} 3`
+    rslt=`./thrust data2 ${COUNT} 10000 3`
     echo $rslt","$COUNT | tee -a thrust.csv
     COUNT=`expr $COUNT + $INTVL` # COUNT をインクリメント
 done
 
 python plot3.py atomic.csv fine-coarse.csv thrust.csv 
-cp plot3.png plot3.iter.png
-
+cp plot3.png plot3.data.png
