fc62903ffe26f4fd1584698a6de3b580d11893f2
Ruo Ando <ando.ruo@gmail.com>
2019-05-21 10:31:39 +0900

    6

--- session/session-category-by-gpu/do.sh
+++ session/session-category-by-gpu/do.sh
@@ -16,16 +16,20 @@ time CUDA_VISIBLE_DEVICES=1 ./traverse6 ./${DATE}
 cp tmp-social-networking tmp-social-networking-${DATE}
 cp tmp-web-advertisements tmp-web-advertisements-${DATE}
 cp tmp-online-storage-and-backup tmp-online-storage-and-backup-${DATE}
+cp tmp-content-delivery-networks tmp-content-delivery-networks-${DATE}
 
 scp tmp-social-networking-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_category_by_gpu/gpu04/
 scp tmp-social-networking-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_category_by_gpu/gpu04/tmp-social-networking-current
 scp tmp-online-storage-and-backup-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_category_by_gpu/gpu04/tmp-online-storage-and-backup-current
+scp tmp-content-delivery-networks-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_category_by_gpu/gpu04/tmp-content-delivery-networks-current
 
 scp tmp-social-networking-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_category_by_gpu/gpu04/
 scp tmp-web-advertisements-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_category_by_gpu/gpu04/
 scp tmp-online-storage-and-backup-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_category_by_gpu/gpu04/
+scp tmp-content-delivery-networks-${DATE} 192.168.72.6:/mnt/sdc/splunk_session_category_by_gpu/gpu04/
 
 rm -rf ${DATE}
 rm -rf tmp-social-networking-${DATE}
 rm -rf tmp-web-advertisements-${DATE}
 rm -rf tmp-online-storage-and-backup-${DATE}
+rm -rf tmp-content-delivery-networks-${DATE}
--- session/session-category-by-gpu/traverse6.cpp
+++ session/session-category-by-gpu/traverse6.cpp
@@ -217,6 +217,13 @@ int traverse_file(char* filename) {
 	    TbbVec3.push_back(s);
 	    TbbVec_category.push_back(3);
 	  }
+   	else if(category == "content-delivery-networks")
+	  {
+	    TbbVec1.push_back(stol(tms));
+	    TbbVec2.push_back(stol(bytes));
+	    TbbVec3.push_back(s);
+	    TbbVec_category.push_back(4);
+	  }
 	else
 	  {
 	    TbbVec1.push_back(stol(tms));
@@ -608,6 +615,51 @@ int main(int argc, char* argv[]) {
     for(start_timestamp = TbbVec1.begin(); start_timestamp != end_timestamp ;++start_timestamp)
       {
 	if((long)*start_category == 3)
+	  {
+	    h_key[counter] = (long)*start_timestamp;
+	    h_value_count[counter] = (long)*start_category;
+	    h_value_bytes[counter] = (long)*start_bytes;
+	    counter++;
+	  }
+	    
+	 start_bytes++;
+	 start_category++;
+      }
+
+    kernel(h_key, h_value_count, h_value_bytes, filename, counter);
+
+    free(h_key);
+    free(h_value_count);
+    free(h_value_bytes);
+
+    /////
+
+    filename = "tmp-content-delivery-networks";
+    category_counter = 0;
+
+    start_category = TbbVec_category.begin();
+    for(start_timestamp = TbbVec1.begin(); start_timestamp != end_timestamp; ++start_timestamp)
+      {    
+	if((long)*start_category == 4)
+	  {
+	    category_counter++;
+	  }
+	start_category++;
+      }
+
+    h_key = (long *)malloc(category_counter*sizeof(long));
+
+    h_value_count = (long *)malloc(category_counter*sizeof(long));
+
+    h_value_bytes = (long *)malloc(category_counter*sizeof(long));
+    
+    start_category = TbbVec_category.begin();
+    start_bytes = TbbVec2.begin();
+
+    counter = 0;
+    for(start_timestamp = TbbVec1.begin(); start_timestamp != end_timestamp ;++start_timestamp)
+      {
+	if((long)*start_category == 4)
 	  {
 	    h_key[counter] = (long)*start_timestamp;
 	    h_value_count[counter] = (long)*start_category;
