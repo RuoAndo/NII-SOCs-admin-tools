eebccea7f546360521f2875750816ca4a0fd7d1b
Ruo Ando <ando.ruo@gmail.com>
2019-10-07 04:02:08 +0000

    nvme

--- nvme/read/read.c
+++ nvme/read/read.c
@@ -3,6 +3,7 @@
 #include "spdk/nvme.h"
 #include "spdk/vmd.h"
 #include "spdk/env.h"
+
 #include <time.h>
 
 #define THREAD_NUM 50
@@ -20,11 +21,6 @@ struct ns_entry {
 	struct spdk_nvme_qpair	*qpair;
 };
 
-typedef struct _thread_arg {
-    int thread_no;
-    int *data;
-} thread_arg_t;
-
 static struct ctrlr_entry *g_controllers = NULL;
 static struct ns_entry *g_namespaces = NULL;
 
@@ -34,14 +30,8 @@ static void
 register_ns(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_ns *ns)
 {
 	struct ns_entry *entry;
-	const struct spdk_nvme_ctrlr_data *cdata;
-
-	cdata = spdk_nvme_ctrlr_get_data(ctrlr);
 
 	if (!spdk_nvme_ns_is_active(ns)) {
-		printf("Controller %-20.20s (%-20.20s): Skipping inactive NS %u\n",
-		       cdata->mn, cdata->sn,
-		       spdk_nvme_ns_get_id(ns));
 		return;
 	}
 
@@ -60,54 +50,63 @@ register_ns(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_ns *ns)
 	       spdk_nvme_ns_get_size(ns) / 1000000000);
 }
 
-struct write_sequence {
+struct hello_world_sequence {
 	struct ns_entry	*ns_entry;
 	char		*buf;
 	unsigned        using_cmb_io;
 	int		is_completed;
-        int             counter; 
 };
 
-static void
-write_complete(void *arg, const struct spdk_nvme_cpl *completion)
-{
-	struct write_sequence	        *sequence = arg;
-	struct ns_entry			*ns_entry = sequence->ns_entry;
-	int				rc;
+typedef struct _thread_arg {
+    int thread_no;
+    int *data;
+} thread_arg_t;
 
+static void
+read_complete(void *arg, const struct spdk_nvme_cpl *completion)
+{
+	struct hello_world_sequence *sequence = arg;
+
+	/* Assume the I/O was successful */
+	sequence->is_completed = 1;
+	/* See if an error occurred. If so, display information
+	 * about it, and set completion value so that I/O
+	 * caller is aware that an error occurred.
+	 */
 	if (spdk_nvme_cpl_is_error(completion)) {
 		spdk_nvme_qpair_print_completion(sequence->ns_entry->qpair, (struct spdk_nvme_cpl *)completion);
 		fprintf(stderr, "I/O error status: %s\n", spdk_nvme_cpl_get_status_string(&completion->status));
-		fprintf(stderr, "Write I/O failed, aborting run\n");
+		fprintf(stderr, "Read I/O failed, aborting run\n");
 		sequence->is_completed = 2;
-		exit(1);
 	}
-	else
-	  {
-	    sequence->is_completed = 1;
-	    sequence->counter++;
-	    // fprintf(stderr, "write_complete() done. \n");
-	  }	
+
+	/*
+	 * The read I/O has completed.  Print the contents of the
+	 *  buffer, free the buffer, then mark the sequence as
+	 *  completed.  This will trigger the hello_world() function
+	 *  to exit its polling loop.
+	 */
+	printf("%s", sequence->buf);
+	spdk_free(sequence->buf);
 }
 
 static void
 thread_func(void *arg)
 {
-        struct ns_entry			*ns_entry;
-	struct write_sequence	        sequence;
+	struct ns_entry			*ns_entry;
+	struct hello_world_sequence	sequence;
 	int				rc;
 
 	long int elapsed_time_1;
 	long int elapsed_time_2;
 	
+	thread_arg_t* targ = (thread_arg_t *)arg;	
 	struct timespec startTime, endTime, sleepTime;
 
-	thread_arg_t* targ = (thread_arg_t *)arg;
-	
 	clock_gettime(CLOCK_REALTIME, &startTime);
 	sleepTime.tv_sec = 0;
-	sleepTime.tv_nsec = 123;
-		
+	sleepTime.tv_nsec = 123;	
+	
 	ns_entry = g_namespaces;
 	while (ns_entry != NULL) {
 		ns_entry->qpair = spdk_nvme_ctrlr_alloc_io_qpair(ns_entry->ctrlr, NULL, 0);
@@ -129,35 +128,28 @@ thread_func(void *arg)
 		if (sequence.using_cmb_io) {
 			printf("INFO: using controller memory buffer for IO\n");
 		} else {
-		        printf("INFO: thread %d using host memory buffer for IO\n", targ->thread_no);
+			printf("INFO: using host memory buffer for IO\n");
 		}
 		sequence.is_completed = 0;
 		sequence.ns_entry = ns_entry;
+	
+		rc = spdk_nvme_ns_cmd_read(ns_entry->ns, ns_entry->qpair, sequence.buf,
+					    targ->thread_no, 
+					    1, 
+					    read_complete, &sequence, 0);
+		if (rc != 0) {
+			fprintf(stderr, "starting write I/O failed\n");
+			exit(1);
+		}
 
-		snprintf(sequence.buf, 0x1000, "[%d]%s", targ->thread_no, "Hello world! - 2019.10.07 \n");
+		while (!sequence.is_completed) {
+			spdk_nvme_qpair_process_completions(ns_entry->qpair, 0);
+		}
 
-		    int rc = spdk_nvme_ns_cmd_write(ns_entry->ns, ns_entry->qpair, sequence.buf,
-						    targ->thread_no, /* LBA start */
-						    1, /* number of LBAs */
-						    write_complete, &sequence, 0);
-		    if (rc != 0) {
-		      fprintf(stderr, "starting write I/O failed\n");
-		      exit(1);
-		    }
-
-		    while (!sequence.is_completed) {
-		      spdk_nvme_qpair_process_completions(ns_entry->qpair, 0);
-		    }		
-		    spdk_nvme_ctrlr_free_io_qpair(ns_entry->qpair);
-
+		spdk_nvme_ctrlr_free_io_qpair(ns_entry->qpair);
 		ns_entry = ns_entry->next;
 	}
 
-	clock_gettime(CLOCK_REALTIME, &endTime);
-
-	// printf("開始時刻　 = %10ld.%09ld\n", startTime.tv_sec, startTime.tv_nsec);
-	// printf("終了時刻　 = %10ld.%09ld\n", endTime.tv_sec, endTime.tv_nsec);
-	
 	// printf("経過実時間 = ");
 	if (endTime.tv_nsec < startTime.tv_nsec) {
 	  elapsed_time_1 = endTime.tv_sec - startTime.tv_sec - 1;
@@ -166,10 +158,62 @@ thread_func(void *arg)
 	  elapsed_time_1 = endTime.tv_sec - startTime.tv_sec;
 	  elapsed_time_2 = endTime.tv_nsec - startTime.tv_nsec;
 	}
-       		
 	printf("THREAD ID %d done. - %10ld.%09ld (sec) \n", targ->thread_no, elapsed_time_1, elapsed_time_2);
-
 }
+  
+
+/*
+static void
+hello_world(void)
+{
+	struct ns_entry			*ns_entry;
+	struct hello_world_sequence	sequence;
+	int				rc;
+
+	ns_entry = g_namespaces;
+	while (ns_entry != NULL) {
+		ns_entry->qpair = spdk_nvme_ctrlr_alloc_io_qpair(ns_entry->ctrlr, NULL, 0);
+		if (ns_entry->qpair == NULL) {
+			printf("ERROR: spdk_nvme_ctrlr_alloc_io_qpair() failed\n");
+			return;
+		}
+
+		sequence.using_cmb_io = 1;
+		sequence.buf = spdk_nvme_ctrlr_alloc_cmb_io_buffer(ns_entry->ctrlr, 0x1000);
+		if (sequence.buf == NULL) {
+			sequence.using_cmb_io = 0;
+			sequence.buf = spdk_zmalloc(0x1000, 0x1000, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
+		}
+		if (sequence.buf == NULL) {
+			printf("ERROR: write buffer allocation failed\n");
+			return;
+		}
+		if (sequence.using_cmb_io) {
+			printf("INFO: using controller memory buffer for IO\n");
+		} else {
+			printf("INFO: using host memory buffer for IO\n");
+		}
+		sequence.is_completed = 0;
+		sequence.ns_entry = ns_entry;
+	
+		rc = spdk_nvme_ns_cmd_read(ns_entry->ns, ns_entry->qpair, sequence.buf,
+					    0, 
+					    1, 
+					    read_complete, &sequence, 0);
+		if (rc != 0) {
+			fprintf(stderr, "starting write I/O failed\n");
+			exit(1);
+		}
+
+		while (!sequence.is_completed) {
+			spdk_nvme_qpair_process_completions(ns_entry->qpair, 0);
+		}
+
+		spdk_nvme_ctrlr_free_io_qpair(ns_entry->qpair);
+		ns_entry = ns_entry->next;
+	}
+}
+*/
 
 static bool
 probe_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
@@ -187,7 +231,7 @@ attach_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
 	int nsid, num_ns;
 	struct ctrlr_entry *entry;
 	struct spdk_nvme_ns *ns;
-	const struct spdk_nvme_ctrlr_data *cdata = spdk_nvme_ctrlr_get_data(ctrlr);
+	const struct spdk_nvme_ctrlr_data *cdata;
 
 	entry = malloc(sizeof(struct ctrlr_entry));
 	if (entry == NULL) {
@@ -197,12 +241,30 @@ attach_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
 
 	printf("Attached to %s\n", trid->traddr);
 
+	/*
+	 * spdk_nvme_ctrlr is the logical abstraction in SPDK for an NVMe
+	 *  controller.  During initialization, the IDENTIFY data for the
+	 *  controller is read using an NVMe admin command, and that data
+	 *  can be retrieved using spdk_nvme_ctrlr_get_data() to get
+	 *  detailed information on the controller.  Refer to the NVMe
+	 *  specification for more details on IDENTIFY for NVMe controllers.
+	 */
+	cdata = spdk_nvme_ctrlr_get_data(ctrlr);
+
 	snprintf(entry->name, sizeof(entry->name), "%-20.20s (%-20.20s)", cdata->mn, cdata->sn);
 
 	entry->ctrlr = ctrlr;
 	entry->next = g_controllers;
 	g_controllers = entry;
 
+	/*
+	 * Each controller has one or more namespaces.  An NVMe namespace is basically
+	 *  equivalent to a SCSI LUN.  The controller's IDENTIFY data tells us how
+	 *  many namespaces exist on the controller.  For Intel(R) P3X00 controllers,
+	 *  it will just be one namespace.
+	 *
+	 * Note that in NVMe, namespace IDs start at 1, not 0.
+	 */
 	num_ns = spdk_nvme_ctrlr_get_num_ns(ctrlr);
 	printf("Using controller %s with %d namespaces.\n", entry->name, num_ns);
 	for (nsid = 1; nsid <= num_ns; nsid++) {
@@ -233,9 +295,34 @@ cleanup(void)
 		free(ctrlr_entry);
 		ctrlr_entry = next;
 	}
+}
 
-	printf("cleanup() done. \n");
-	
+static void
+usage(const char *program_name)
+{
+	printf("%s [options]", program_name);
+	printf("\n");
+	printf("options:\n");
+	printf(" -V         enumerate VMD\n");
+}
+
+static int
+parse_args(int argc, char **argv)
+{
+	int op;
+
+	while ((op = getopt(argc, argv, "V")) != -1) {
+		switch (op) {
+		case 'V':
+			g_vmd = true;
+			break;
+		default:
+			usage(argv[0]);
+			return 1;
+		}
+	}
+
+	return 0;
 }
 
 int main(int argc, char **argv)
@@ -248,6 +335,17 @@ int main(int argc, char **argv)
 	pthread_t handle[THREAD_NUM];
 	thread_arg_t targ[THREAD_NUM];
 	
+	rc = parse_args(argc, argv);
+	if (rc != 0) {
+		return rc;
+	}
+
+	/*
+	 * SPDK relies on an abstraction around the local environment
+	 * named env that handles memory allocation and PCI device operations.
+	 * This library must be initialized first.
+	 *
+	 */
 	spdk_env_opts_init(&opts);
 	opts.name = "hello_world";
 	opts.shm_id = 0;
@@ -263,6 +361,13 @@ int main(int argc, char **argv)
 			" Some NVMe devices can be unavailable.\n");
 	}
 
+	/*
+	 * Start the SPDK NVMe enumeration process.  probe_cb will be called
+	 *  for each NVMe controller found, giving our application a choice on
+	 *  whether to attach to each controller.  attach_cb will then be
+	 *  called for each controller after the SPDK NVMe driver has completed
+	 *  initializing the controller we chose to attach.
+	 */
 	rc = spdk_nvme_probe(NULL, NULL, probe_cb, attach_cb, NULL);
 	if (rc != 0) {
 		fprintf(stderr, "spdk_nvme_probe() failed\n");
