ec3ea40efe283487eda0ccbc71a9fd5bcb368675
Ruo Ando <ando.ruo@gmail.com>
2019-11-14 11:53:47 +0900

    arg

---
+++ gpu/tf/lstm.py
@@ -0,0 +1,235 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+import sys
+import statistics
+
+np.random.seed(0)
+tf.set_random_seed(1234)
+
+def zscore(x, axis = None):
+    xmean = x.mean(axis=axis, keepdims=True)
+    xstd  = np.std(x, axis=axis, keepdims=True)
+    zscore = (x-xmean)/xstd
+    return zscore
+
+def min_max(l):
+    l_min = min(l)
+    l_max = max(l)
+    return [(i - l_min) / (l_max - l_min) for i in l]
+
+def standardization(l):
+    l_mean = statistics.mean(l)
+    l_stdev = statistics.stdev(l)
+    return [(i - l_mean) / l_stdev for i in l]
+
+def standardization_p(l):
+    l_mean = statistics.mean(l)
+    l_pstdev = statistics.pstdev(l)
+    return [(i - l_mean) / l_pstdev for i in l]
+
+def forward(x, n_batch, maxlen=None, n_hidden=None, n_out=None):
+    def weight_variable(shape):
+        initial = tf.truncated_normal(shape, stddev=0.01)
+        return tf.Variable(initial)
+
+    def bias_variable(shape):
+        initial = tf.zeros(shape, dtype=tf.float32)
+        return tf.Variable(initial)
+
+    cell = tf.nn.rnn_cell.LSTMCell(n_hidden, forget_bias=1.0)
+    initial_state = cell.zero_state(n_batch, tf.float32)
+
+    state = initial_state
+    outputs = []  # 過去の隠れ層の出力を保存
+    with tf.variable_scope('LSTM'):
+        for t in range(maxlen):
+            if t > 0:
+                tf.get_variable_scope().reuse_variables()
+            (cell_output, state) = cell(x[:, t, :], state)
+            outputs.append(cell_output)
+
+    output = outputs[-1]
+
+    V = weight_variable([n_hidden, n_out])
+    c = bias_variable([n_out])
+    y = tf.matmul(output, V) + c  # 線形活性
+
+    return y
+
+
+def loss(y, t):
+    mse = tf.reduce_mean(tf.square(y - t))
+    return mse
+
+
+def training(loss):
+    optimizer = \
+        tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.9, beta2=0.999)
+
+    train_step = optimizer.minimize(loss)
+    return train_step
+
+
+class EarlyStopping():
+    def __init__(self, patience=0, verbose=0):
+        self._step = 0
+        self._loss = float('inf')
+        self.patience = patience
+        self.verbose = verbose
+
+    def validate(self, loss):
+        if self._loss < loss:
+            self._step += 1
+            if self._step > self.patience:
+                if self.verbose:
+                    print('early stopping')
+                return True
+        else:
+            self._step = 0
+            self._loss = loss
+
+        return False
+
+
+if __name__ == '__main__':
+
+    #length_of_sequences = 2 * T
+    #maxlen = 25
+
+    args = sys.argv
+    test_data = open(args[1], "r")
+    #test_data = open("sin-out.txt", "r")
+    f = []
+    
+    line_counter = 0
+    for line in test_data:
+
+        if line_counter == 0:
+            line_counter = line_counter + 1
+            continue
+
+        tmp_item = line.split(",")
+
+        f.append(float(tmp_item[1]))
+        
+    print f
+      
+    test_data.close()
+
+    f = standardization_p(f)
+
+    T = 430
+    length_of_sequences = T
+    maxlen = 240
+    batch_size = 30
+    
+    data = []
+    target = []
+
+    for i in range(0, length_of_sequences - maxlen + 1):
+        data.append(f[i: i + maxlen])
+        target.append(f[i + maxlen])
+
+    X = np.array(data).reshape(len(data), maxlen, 1)
+    Y = np.array(target).reshape(len(data), 1)
+
+    # データ設定
+    N_train = int(len(data) * 0.9)
+    N_validation = len(data) - N_train
+
+    X_train, X_validation, Y_train, Y_validation = \
+        train_test_split(X, Y, test_size=N_validation)
+
+    '''
+    モデル設定
+    '''
+    n_in = len(X[0][0])  # 1
+    n_hidden = 30
+    n_out = len(Y[0])  # 1
+
+    x = tf.placeholder(tf.float32, shape=[None, maxlen, n_in])
+    t = tf.placeholder(tf.float32, shape=[None, n_out])
+    n_batch = tf.placeholder(tf.int32, shape=[])
+
+    y = forward(x, n_batch, maxlen=maxlen, n_hidden=n_hidden, n_out=n_out)
+    loss = loss(y, t)
+    train_step = training(loss)
+
+    early_stopping = EarlyStopping(patience=10, verbose=1)
+    history = {
+        'val_loss': []
+    }
+
+    '''
+    モデル学習
+    '''
+    epochs = 500
+    batch_size = 10
+
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+
+    n_batches = N_train // batch_size
+
+    for epoch in range(epochs):
+        X_, Y_ = shuffle(X_train, Y_train)
+
+        for i in range(n_batches):
+            start = i * batch_size
+            end = start + batch_size
+
+            sess.run(train_step, feed_dict={
+                x: X_[start:end],
+                t: Y_[start:end],
+                n_batch: batch_size
+            })
+
+        # 検証データを用いた評価
+        val_loss = loss.eval(session=sess, feed_dict={
+            x: X_validation,
+            t: Y_validation,
+            n_batch: N_validation
+        })
+
+        history['val_loss'].append(val_loss)
+        print('epoch:', epoch,
+              ' validation loss:', val_loss)
+
+        # Early Stopping チェック
+        if early_stopping.validate(val_loss):
+            break
+
+    '''
+    出力を用いて予測
+    '''
+    truncate = maxlen
+    Z = X[:1]  # 元データの最初の一部だけ切り出し
+
+    original = [f[i] for i in range(maxlen)]
+    predicted = [None for i in range(maxlen)]
+
+    for i in range(length_of_sequences - maxlen + 1):
+        # 最後の時系列データから未来を予測
+        z_ = Z[-1:]
+        y_ = y.eval(session=sess, feed_dict={
+            x: Z[-1:],
+            n_batch: 1
+        })
+        # 予測結果を用いて新しい時系列データを生成
+        sequence_ = np.concatenate(
+            (z_.reshape(maxlen, n_in)[1:], y_), axis=0) \
+            .reshape(1, maxlen, n_in)
+        Z = np.append(Z, sequence_, axis=0)
+        predicted.append(y_.reshape(-1))
+
+    plt.figure()
+    plt.plot(f, linestyle='dotted', color='#aaaaaa')
+    plt.plot(original, linestyle='dashed', color='black')
+    plt.plot(predicted, color='black')
+    plt.show()
