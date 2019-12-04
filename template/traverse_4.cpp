5961427a48a22a78d7c3183e2af271c75d5de4ac
Ruo Ando <ando.ruo@gmail.com>
2019-12-04 12:24:30 +0900

    traverse

---
+++ template/traverse_4.cpp
@@ -0,0 +1,419 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <alloca.h>
+#include "timer.h"
+
+#include <iostream>
+#include <fstream>
+
+#include<boost/spirit/include/qi.hpp>
+
+#include "csv.hpp"
+
+#include "tbb/task_scheduler_init.h"
+#include "tbb/concurrent_hash_map.h"
+#include "tbb/blocked_range.h"
+#include "tbb/parallel_for.h"  
+
+#define WORKER_THREAD_NUM (2)
+#define MAX_QUEUE_NUM (1024)
+#define END_MARK_FNAME   "///"
+#define END_MARK_FLENGTH 3
+
+typedef struct _tms_string {
+    // std::string timestamp_string[WORKER_THREAD_NUM];
+    std::string timestamp_string;
+    pthread_mutex_t mutex;    
+} tms_string_t;
+tms_string_t tms_string;
+
+
+namespace qi = boost::spirit::qi;
+using namespace std;
+
+using qi::int_;
+using qi::parse;
+using qi::char_;
+
+using namespace tbb;
+
+struct HashCompare {
+  static size_t hash( unsigned long long x ) {
+    return (size_t)x;
+  }
+  static bool equal( unsigned long long x, unsigned long long y ) {
+    return x==y;
+  }
+};
+
+typedef concurrent_hash_map<unsigned long long, int, HashCompare> TimeStamp_msec;
+static TimeStamp_msec timestamp_msec;    
+
+typedef struct _result {
+    int num;
+    char* fname;
+    pthread_mutex_t mutex;    
+} result_t;
+result_t result;
+
+typedef struct _queue {
+    char* fname[MAX_QUEUE_NUM];
+    int flength[MAX_QUEUE_NUM];
+    int rp, wp;
+    int remain;
+    pthread_mutex_t mutex;
+    pthread_cond_t not_full;
+    pthread_cond_t not_empty;
+} queue_t;
+
+typedef struct _thread_arg {
+    int cpuid;
+    int thread_id;
+    queue_t* q;
+    char* srchstr;
+    char* dirname;
+    int filenum;
+} thread_arg_t;
+
+int traverse_file(char* filename, char* srchstr, int thread_id) {
+    char buf[1024];
+    int n = 0;
+    int i;
+
+    int counter = 0;
+    double result;
+    
+    const string sfile = std::string(filename); 
+    vector<vector<string>> sdata; 
+
+    int thread_id_local = thread_id;
+    std::string local_string = "[local string]:";
+    
+    try {
+      Csv objCsv(sfile);
+      if (!objCsv.getCsv(sdata)) {
+	cout << "read ERROR" << endl;
+	return 1;
+      }
+    }
+    catch (...) {
+      cout << "EXCEPTION (READ)" << endl;
+      return 1;
+    }
+
+    for (unsigned int row = 0; row < sdata.size(); row++) {
+        vector<string> rec = sdata[row];
+	     
+	std::string timestamp = rec[0];
+	cout << timestamp << endl;
+
+	pthread_mutex_lock(&tms_string.mutex);
+
+	/*
+	  if( timestamp_string[thread_id_local].length() > 0)
+	    timestamp_string[thread_id_local].clear();
+	*/	
+
+	tms_string.timestamp_string.clear();
+	
+	std::string::iterator first = timestamp.begin(), last = timestamp.end();
+
+	auto str = [=](int x){
+	  
+	  ostringstream ss;
+	  ss << x;
+
+	  if(ss.str().length() == 1)
+	    {
+	      tms_string.timestamp_string = tms_string.timestamp_string + "0" + ss.str();
+	    }
+	  else
+	    {
+	      tms_string.timestamp_string = tms_string.timestamp_string + ss.str();
+	    }
+
+	  std::cout << "lamba applied to:" << x << ":" << "threadID:" << thread_id_local << std::endl;
+	};
+
+	parse(
+	      first,
+	      last,
+	      '"' >>
+	      int_[str] >>
+	      '/' >>
+	      int_[str] >> 
+	      '/' >>
+	      int_[str] >>
+	      ' ' >>
+	      int_[str] >>
+	      ':' >>
+	      int_[str] >>
+	      ':' >>
+	      int_[str] 
+	      );
+
+	std::cout << "threadID" << thread_id_local << ":" << filename << ":" << tms_string.timestamp_string << std::endl;
+
+	unsigned long long timestamp_ull = stoull(tms_string.timestamp_string);	
+	TimeStamp_msec::accessor a;
+	timestamp_msec.insert(a, timestamp_ull);
+	a->second += 1;
+     
+	pthread_mutex_unlock(&tms_string.mutex);
+	
+    }
+
+    // printf("%s \n", filename);
+
+    return 0;
+}
+
+void initqueue(queue_t* q) {
+    int i;
+    q->rp = q->wp = q->remain = 0;
+    for (i = 0; i < MAX_QUEUE_NUM; ++i) q->fname[i] = NULL;
+    pthread_mutex_init(&q->mutex,    NULL);
+    pthread_cond_init(&q->not_full,  NULL);
+    pthread_cond_init(&q->not_empty, NULL);
+    return;
+}
+
+void enqueue(queue_t* q, char* path, int size) {
+  
+    pthread_mutex_lock(&q->mutex);
+    while (q->remain == MAX_QUEUE_NUM) {
+        pthread_cond_wait(&q->not_full, &q->mutex);
+    }
+    char** fname = (char**)&q->fname[q->wp];
+    if (*fname != NULL) free(*fname);
+    *fname = (char*)malloc(size);
+    strcpy(*fname, path);
+    q->flength[q->wp] = size;
+    q->wp++; q->remain++;
+
+    if (q->wp == MAX_QUEUE_NUM) q->wp = 0;
+
+    pthread_cond_signal(&q->not_empty);
+    pthread_mutex_unlock(&q->mutex);
+    return;
+}
+
+void dequeue(queue_t* q, char** fname, int* flen) {
+
+    pthread_mutex_lock(&q->mutex);
+    while (q->remain == 0) 
+        pthread_cond_wait(&q->not_empty, &q->mutex);
+
+    *flen  = q->flength[q->rp];
+    if (*fname != NULL) free(*fname);
+    *fname = (char*)malloc(*flen);
+    strcpy(*fname, q->fname[q->rp]);
+    q->rp++; q->remain--;
+    if (q->rp == MAX_QUEUE_NUM) q->rp = 0;
+    pthread_cond_signal(&q->not_full);
+    pthread_mutex_unlock(&q->mutex);
+    if (strcmp(*fname,"")==0) printf("rp=%d\n", q->rp-1);
+    return;
+}
+
+int traverse_dir_thread(queue_t* q, char* dirname) {
+    static int cnt = 0;
+    struct dirent* dent;
+    DIR* dd = opendir(dirname);
+
+    if (dd == NULL) {
+        printf("Could not open the directory %s\n", dirname); return 0;
+    }
+
+    while ((dent = readdir(dd)) != NULL) {
+        if (strncmp(dent->d_name, ".",  2) == 0) continue;
+        if (strncmp(dent->d_name, "..", 3) == 0) continue;	
+
+        int size = strlen(dirname) + strlen(dent->d_name) + 2;
+#if 0
+        char* path = (char*)malloc(size);
+        sprintf(path, "%s/%s", dirname, dent->d_name);
+
+        struct stat fs;
+        if (stat(path, &fs) < 0)
+            continue;
+        else {
+            if (S_ISDIR(fs.st_mode))
+                traverse_dir_thread(q, path);
+            else if (S_ISREG(fs.st_mode)) {
+                enqueue(q, path, size);
+                cnt++;
+            }
+        }
+#else
+        {
+            char* path = (char*)alloca(size);
+            sprintf(path, "%s/%s", dirname, dent->d_name);
+
+            struct stat fs;
+            if (stat(path, &fs) < 0)
+                continue;
+            else {
+                if (S_ISDIR(fs.st_mode))
+                    traverse_dir_thread(q, path);
+                else if (S_ISREG(fs.st_mode)) {
+                    enqueue(q, path, size);
+                    cnt++;
+                }
+            }
+        }
+#endif
+    }
+    closedir(dd);
+    return cnt;
+}
+
+void master_func(thread_arg_t* arg) {
+    queue_t* q = arg->q;
+    int i;
+    arg->filenum = traverse_dir_thread(q, arg->dirname);
+    /* enqueue END_MARK */
+    for (i = 0; i < WORKER_THREAD_NUM; ++i) 
+        enqueue(q, END_MARK_FNAME, END_MARK_FLENGTH);
+    return;
+}
+
+void worker_func(thread_arg_t* arg) {
+    int flen;
+    char* fname = NULL;
+    queue_t* q = arg->q;
+    char* srchstr = arg->srchstr;
+
+    int thread_id = arg->thread_id;
+
+#ifdef __CPU_SET
+    cpu_set_t mask;    
+    __CPU_ZERO(&mask);
+    __CPU_SET(arg->cpuid, &mask);
+    if (sched_setaffinity(0, sizeof(mask), &mask) == -1)
+        printf("WARNING: faild to set CPU affinity...\n");
+#endif
+
+#if 0
+    while (1) {
+        int n;
+
+        dequeue(q, &fname, &flen));
+
+        if (strncmp(fname, END_MARK_FNAME, END_MARK_FLENGTH + 1) == 0)
+            break;
+
+        n = traverse_file(fname, srchstr, thread_id);
+        pthread_mutex_lock(&result.mutex);
+
+        if (n > result.num) {
+            result.num = n;
+            if (result.fname != NULL) free(result.fname);
+            result.fname = (char*)malloc(flen);
+            strcpy(result.fname, fname);
+        }
+        pthread_mutex_unlock(&result.mutex);
+    }
+#else
+    char* my_result_fname;
+    int my_result_num = 0;
+    int my_result_len = 0;
+    while (1) {
+        int n;
+
+        dequeue(q, &fname, &flen);
+
+        if (strncmp(fname, END_MARK_FNAME, END_MARK_FLENGTH + 1) == 0)
+            break;
+
+        n = traverse_file(fname, srchstr, thread_id);
+
+        if (n > my_result_num) {
+            my_result_num = n;
+            my_result_len = flen;
+            my_result_fname = (char*)alloca(flen);
+            strcpy(my_result_fname, fname);
+        }
+    }
+    pthread_mutex_lock(&result.mutex);
+    if (my_result_num > result.num) {
+        result.num = my_result_num;
+	if (result.fname != NULL) free(result.fname);
+        result.fname = (char*)malloc(my_result_len);
+        strcpy(result.fname, my_result_fname);	
+    }
+    pthread_mutex_unlock(&result.mutex);
+#endif
+    return;
+}
+
+void print_result(thread_arg_t* arg) {
+    if (result.num) {
+        printf("Total %d files\n", arg->filenum);
+        printf("Max include file: %s[include %d]\n", result.fname, result.num);	
+    }
+    return;
+}
+
+int main(int argc, char* argv[]) {
+    int i;
+    int thread_num = 1 + WORKER_THREAD_NUM;
+    unsigned int t, travdirtime;
+    queue_t q;
+    thread_arg_t targ[thread_num];
+    pthread_t master;
+    pthread_t worker[thread_num];
+    int cpu_num;
+
+    if (argc != 2) {
+      printf("Usage: ./traverse [DIR]\n"); return 0;
+    }
+    cpu_num = sysconf(_SC_NPROCESSORS_CONF);
+
+    initqueue(&q);
+
+    for (i = 0; i < thread_num; ++i) {
+        targ[i].q = &q;
+        // targ[i].srchstr = argv[1];
+        targ[i].dirname = argv[1];
+        targ[i].filenum = 0;
+	targ[i].thread_id = i;
+        targ[i].cpuid = i%cpu_num;
+    }
+    result.fname = NULL;
+
+    start_timer(&t);
+
+    pthread_mutex_init(&result.mutex, NULL);
+
+    pthread_create(&master, NULL, (void*)master_func, (void*)&targ[0]);
+    for (i = 1; i < thread_num; ++i)
+        pthread_create(&worker[i], NULL, (void*)worker_func, (void*)&targ[i]);
+	
+    for (i = 1; i < thread_num; ++i) 
+        pthread_join(worker[i], NULL);
+
+    travdirtime = stop_timer(&t);
+    print_timer(travdirtime);
+
+    print_result(&targ[0]);
+    for (i = 1; i < thread_num; ++i) {
+        if ((targ[i].q)->fname[i] != NULL) free((targ[i].q)->fname[i]);
+    }
+    if(result.fname != NULL) free(result.fname);
+
+    for( TimeStamp_msec::iterator i=timestamp_msec.begin(); i!=timestamp_msec.end(); ++i )
+      {
+	cout << i->first << "," << i->second << endl;
+	// counter++;
+      }                         
+
+    return 0;
+}
