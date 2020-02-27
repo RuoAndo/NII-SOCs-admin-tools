07974f7c50af0c9d970ea3b2bf81feff46e7e7f1
Ruo Ando <ando.ruo@gmail.com>
2020-02-27 09:58:06 +0900

    rm

--- template/rm.sh
+++ template/rm.sh
@@ -1,7 +1,20 @@
 RMDATE=`date --date '4 day ago' +%Y%m%d`
 echo $RMDATE
-rm -rf /data1/$RMDATE
+rm -rf $RMDATE
 
-if [[ ! -e /data1/$RMDATE ]]; then
+if [[ ! -e ./$RMDATE ]]; then
   echo "OK:"$RMDATE
 fi
+
+RMDATE=`date --date '3 day ago' +%Y%m%d`
+echo $RMDATE
+
+while read line; do
+    echo egress_${line}_${RMDATE}
+    
+    rm -rf egress_${line}_${RMDATE}
+    rm -rf egress_${line}/*_${RMDATE}
+    rm -rf ingress_${line}_${RMDATE}
+    rm -rf ingress_${line}/*_${RMDATE}
+
+done < university-list
