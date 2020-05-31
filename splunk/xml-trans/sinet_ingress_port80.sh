2d4c6d4c40f1e97bfbeb78f0b0a90be1ad38811e
Ruo Ando <ando.ruo@gmail.com>
2020-05-31 14:27:05 +0900

    auto

--- splunk/xml-trans/sinet_ingress_port80.sh
+++ splunk/xml-trans/sinet_ingress_port80.sh
@@ -1,5 +1,4 @@
 ./1-1.sh | grep -v egress > parameter-list-ingress-port80
 head -n 5 parameter-list-ingress-port80
 ./gen_streamstats_chart_all_ingress_port80.sh parameter-list-ingress-port80
-head -n 5 streamstats_template_ingress_port80.xml
-
+head -n 5 streamstats_chart_all_ingress_port80.xml
