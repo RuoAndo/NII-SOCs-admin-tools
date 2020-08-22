7b02c5c4ad1587fb135014aa3ed1ea91ef72b0cc
Ruo Ando <ando.ruo@gmail.com>
2020-08-22 20:33:55 +0900

    skipper

---
+++ boost/11-3-2.cpp
@@ -0,0 +1,20 @@
+#include <boost/spirit/include/qi.hpp>
+#include <string>
+#include <iostream>
+
+using namespace boost::spirit;
+
+int main()
+{
+  std::string s;
+  std::getline(std::cin, s);
+  auto it = s.begin();
+  bool match = qi::phrase_parse(it, s.end(), ascii::digit, ascii::space,
+    qi::skip_flag::dont_postskip);
+  *it--;
+  std::cout << std::boolalpha << match << ":" << *it << '\n';
+  *it++;
+  if (it != s.end())
+    std::cout << std::string{it, s.end()} << '\n';
+}
+
