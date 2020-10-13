52162513ab712668cbf37d91f7e31cf022c7f8f0
Ruo Ando <ando.ruo@gmail.com>
2020-10-13 20:02:18 +0900

    rm v4v6

--- detection/misc/v4v6/install.sh
+++
@@ -1,5 +0,0 @@
-yum install java-1.8.0*
-wget wget http://ftp.jaist.ac.jp/pub/apache/maven/maven-3/3.3.9/source/apache-maven-3.3.9-src.tar.gz
-export PATH=$PATH:/home/ec2-user/nii-cyber-security-admin/detection/misc/v4v6/apache-maven-3.3.9/bin
-
-
