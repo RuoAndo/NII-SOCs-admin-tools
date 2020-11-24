120d9a683418fc40a2c791a55cb325fcdb1e4ba4
Ruo Ando <ando.ruo@gmail.com>
2020-11-24 17:20:32 +0900

    plot

--- plot/5.py
+++ plot/5.py
@@ -13,6 +13,7 @@ args = sys.argv
 x = []
 y = []
 
+#f = open('1.csv', 'r')
 f = open(args[1], 'r')
 
 line = f.readline()
@@ -33,13 +34,20 @@ for i in x:
     print(x[counter] + "," + str(y[counter]))
     counter = counter + 1
 
+#ax = plt.subplot(111)
+#ax.plot(x, y, linewidth=1)
+
+#plt.plot(x,y)
+#plt.show()
+
 dates = [dateutil.parser.parse(s) for s in x]
 
 plt.subplots_adjust(bottom=0.2)
 plt.xticks( rotation= 80 )
 ax=plt.gca()
-
+#xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
 xfmt = md.DateFormatter('%Y-%m-%d %H:%M')
 ax.xaxis.set_major_formatter(xfmt)
+#plt.plot(dates[0:10],plt_data[0:10], "o-")
 plt.plot(dates,y)
 plt.show()
