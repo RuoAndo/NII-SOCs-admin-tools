64353eefbfe4c9e596474e6ee05e5c201e4dc618
Ruo Ando <ando.ruo@gmail.com>
2022-11-04 19:00:29 +0900

    do

--- geoip/pthread/distance/do.sh
+++ geoip/pthread/distance/do.sh
@@ -1,10 +1,10 @@
-#./build.sh multi_measure_2
+#time ./do.sh 0 gs.txt 
+./build.sh multi_measure_2
 
 rm -rf log
 touch log
 
 MINDIS=$1
-
 time ./multi_measure_2 box/a/ $MINDIS $2 | tee -a log
 time ./multi_measure_2 box/b/ $MINDIS $2 | tee -a log
 time ./multi_measure_2 box/c/ $MINDIS $2 | tee -a log
@@ -12,4 +12,4 @@ time ./multi_measure_2 box/d/ $MINDIS $2 | tee -a log
 time ./multi_measure_2 box/e/ $MINDIS $2 | tee -a log
 time ./multi_measure_2 box/f/ $MINDIS $2 | tee -a log
 time ./multi_measure_2 box/g/ $MINDIS $2 | tee -a log
-time ./multi_measure_2 box/h/ $MINDIS gx.txt | tee -a log
+time ./multi_measure_2 box/h/ $MINDIS $2 | tee -a log
