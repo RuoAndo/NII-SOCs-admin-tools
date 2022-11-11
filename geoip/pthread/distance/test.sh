99c6abe51d6b97dae2089d177255b1a23131ad65
Ruo Ando <ando.ruo@gmail.com>
2022-11-11 10:13:52 +0900

    test

--- geoip/pthread/distance/test.sh
+++ geoip/pthread/distance/test.sh
@@ -1,6 +1,14 @@
 time ./do.sh $1 $2 
 grep tbb log | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3},[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d ":" -f 2 | cut -d "," -f 1 | sort | uniq -c  | awk '{print $2,$1}' | sort -t " " -k 2n > result.${2}.${1}
 
+grep tbb log | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3},[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d ":" -f 2 | cut -d "," -f 1 | sort | uniq -c  | awk '{print $2","$1}' | sort -t "," -k 2n > csv.${2}.${1}
+
+
+rm -rf tmp
+touch tmp
+
+echo "lat,lng,count" > tmp 
+
 while read line; do
     #echo $line
     ip=`echo $line | cut -d " " -f 1`
@@ -8,7 +16,7 @@ while read line; do
     #echo $ip
     latlng=`python geoip-test.py $ip`
 
-    echo $latlng","$count
+    echo $latlng","$count | tee -a tmp
     #echo $count","$latlng
     
 done < result.${2}.${1}
