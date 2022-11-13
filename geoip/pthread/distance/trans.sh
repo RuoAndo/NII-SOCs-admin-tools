93e56bb4d3ec4bf1a54bb053de181fc45dc60988
Ruo Ando <ando.ruo@gmail.com>
2022-11-14 01:41:45 +0900

    tran

--- geoip/pthread/distance/trans.sh
+++ geoip/pthread/distance/trans.sh
@@ -1,4 +1,9 @@
 while read line; do
-    echo $line | sed -re 's/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/*.*.*.*/g' 
+    #echo $line | sed -re 's/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/*.*.*.*/g' 
+    IP=`echo $line | cut -d "," -f 1 | awk -F "." '{print $1.".*."$2.".*"}'`
+    CNT=`echo $line | cut -d "," -f 2` # | awk -F "." '{print $1.".*."$2.".*"}'`
+
+    echo $IP","$CNT
+    
 done < $1
 
