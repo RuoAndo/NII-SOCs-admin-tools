e77b9d581e124f9fd5f721e18cd77d3bccecad19
Ruo Ando <ando.ruo@gmail.com>
2022-12-07 23:01:00 +0900

    3

--- geoip/pthread/distance/plotter3.py
+++ geoip/pthread/distance/plotter3.py
@@ -5,6 +5,9 @@ import numpy as np
 
 args = sys.argv
 
+if len(args) < 4:
+	quit()
+	
 fig = plt.figure()
 
 data1 = pd.read_csv(args[1],encoding = 'UTF8')
@@ -12,15 +15,18 @@ data1
 
 data1_x = data1[data1.columns[0]]
 data1_y = data1[data1.columns[1]]
-ax1 = fig.add_subplot(3, 1, 1)
+#ax1 = fig.add_subplot(2, 1, 1, xscale="log")
+ax1 = fig.add_subplot(2, 1, 1)
 
-ax1.set_title("distance = 0 KM",fontsize=9)
+titlestr = "distance = 0 KM : " + args[3]
+
+ax1.set_title(titlestr,fontsize=12)
 ax1.set_xlabel("IP")
 ax1.set_ylabel("density") 
 ax1.plot(data1_x, data1_y)
 
-ticks = 10
-plt.xticks(np.arange(0, len(data1_x), ticks), data1_x[::ticks], rotation=40, fontsize=6)
+ticks = 5
+plt.xticks(np.arange(0, len(data1_x), ticks), data1_x[::ticks], rotation=40, fontsize=10)
 
 ####
 
@@ -29,33 +35,23 @@ data2
 
 data2_x = data2[data2.columns[0]]
 data2_y = data2[data2.columns[1]]
-ax2 = fig.add_subplot(3, 1, 2)
 
-ax2.set_title("distance < 500 KM",fontsize=9)
+#ax2 = fig.add_subplot(2, 1, 2, yscale="log")
+ax2 = fig.add_subplot(2, 1, 2)
+
+titlestr = "distance = 0 KM : " + args[4]
+ax2.set_title(titlestr,fontsize=12)
 ax2.set_xlabel("IP")
-ax2.set_ylabel("density") 
+ax2.set_ylabel("density")
+
+#ax2.set_aspect('equal')
+
 ax2.plot(data2_x, data2_y)
 
-ticks = 10
-plt.xticks(np.arange(0, len(data2_x), ticks), data2_x[::ticks], rotation=40, fontsize=6)
-
+#ticks = 5
+plt.xticks(np.arange(0, len(data2_x), ticks), data2_x[::ticks], rotation=40, fontsize=10)
 
 ####
-
-data3 = pd.read_csv(args[3],encoding = 'UTF8')
-data3
-
-data3_x = data3[data3.columns[0]]
-data3_y = data3[data3.columns[1]]
-ax3 = fig.add_subplot(3, 1, 3)
-
-ax3.set_title("distance < 1000 KM",fontsize=9)
-ax3.set_xlabel("IP")
-ax3.set_ylabel("density") 
-ax3.plot(data3_x, data3_y)
-
-ticks = 10
-plt.xticks(np.arange(0, len(data3_x), ticks), data3_x[::ticks], rotation=40, fontsize=6)
 
 fig.subplots_adjust()
 fig.tight_layout()
